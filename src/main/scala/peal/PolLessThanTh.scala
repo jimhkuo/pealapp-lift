42d1382114ebeb9ea7bce873a67d4fe6e2615247
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-03 13:39:22 +0100

    setting up synthesis and simple test

--- src/main/scala/peal/PolLessThanTh.scala
+++ src/main/scala/peal/PolLessThanTh.scala
@@ -3,5 +3,9 @@ package peal
 import peal.synthesis.DefaultSet
 
 class PolLessThanTh(pol: Pol, th: Double) extends DefaultSet {
-  def synthesis: String = ""
+  def synthesis: String = pol.defaultScore match {
+    case s if s > th => ""
+    case _ =>
+      pol.rules.map(_.q.name).foldLeft("")((rules, name) => "!" + name + " ")
+  }
 }
--- src/test/scala/peal/PolLessThanThTest.scala
+++ src/test/scala/peal/PolLessThanThTest.scala
@@ -6,14 +6,22 @@ import org.scalatest.junit.ShouldMatchersForJUnit
 class PolLessThanThTest extends ShouldMatchersForJUnit {
 
   @Test
-  def testCanDoSimpleSynthesis() {
-    val q = new Predicate("q")
-    val p = new Pol(List(new Rule(q, 0.5)), 0)
+  def testCanDoSimpleSynthesisWhenDefaultGreaterThanTh() {
+    val q = new Predicate("q1")
+    val p = new Pol(List(new Rule(q, 0.5)), 1)
+    val defaultSet = new PolLessThanTh(p, 0.6)
+    defaultSet.synthesis should be("") // in CNF
+  }
 
+  @Test
+  def testCanDoSimpleSynthesisWhenDefaultLessThanTh() {
+    val q = new Predicate("q1")
+    val p = new Pol(List(new Rule(q, 0.5)), 0)
     val defaultSet = new PolLessThanTh(p, 0.6)
 
-    println(defaultSet.synthesis)
+    defaultSet.synthesis should be("!q1 ") // in CNF
 
+    println(defaultSet.synthesis)
   }
 
 }
