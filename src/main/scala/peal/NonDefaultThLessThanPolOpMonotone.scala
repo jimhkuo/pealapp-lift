a01e9ae42fd42ef0a8d46cddb9bb2a2c57bba216
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-04 09:11:25 +0100

    working on M1

--- src/main/scala/peal/NonDefaultThLessThanPolOpMonotone.scala
+++ src/main/scala/peal/NonDefaultThLessThanPolOpMonotone.scala
@@ -6,7 +6,12 @@ import peal.synthesis.NonDefaultSet
 class NonDefaultThLessThanPolOpMonotone(pol: Pol, th: Double) extends NonDefaultSet {
   def synthesis: String = ""
 
-  def enumOne(x: Set, sum: Double, index: Integer): Set = {
+  def enumOne(): Set[Set[String]] = {
+    //    val sortedPol =
+    Set()
+  }
+
+  private def enumOne(x: Set[String], sum: Double, index: Integer): Set[String] = {
     Set()
   }
 }
--- src/test/scala/peal/NonDefaultThLessThanPolOpMonotoneTest.scala
+++ src/test/scala/peal/NonDefaultThLessThanPolOpMonotoneTest.scala
@@ -13,4 +13,22 @@ class NonDefaultThLessThanPolOpMonotoneTest extends ShouldMatchersForJUnit {
     //assume M1 is the whole set
     pSet.synthesis should be("q1") // in CNF
   }
+
+  @Test
+  def testCanComputeM1AsPerExample2InSynthesisPdf() {
+    val p = new Pol(List(new Rule(new Predicate("q5"), 0.5),
+      new Rule(new Predicate("q3"), 0.2),
+      new Rule(new Predicate("q4"), 0.3),
+      new Rule(new Predicate("q2"), 0.2),
+      new Rule(new Predicate("q1"), 0.1)), 1)
+    val pSet = new NonDefaultThLessThanPolOpMonotone(p, 0.5)
+
+    pSet.enumOne() should be(Set(Set("q4", "q5"),
+      Set("q3", "q5"),
+      Set("q2", "q5"),
+      Set("q2", "q3", "q4"),
+      Set("q1", "q3", "q4"),
+      Set("q1", "q2", "q4")))
+
+  }
 }
