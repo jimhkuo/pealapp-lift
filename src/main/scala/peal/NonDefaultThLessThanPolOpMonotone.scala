a371f72bba7448e4792961d0e2dc094f465e7f0e
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-04 11:02:06 +0100

    working on M1, almost done

--- src/main/scala/peal/NonDefaultThLessThanPolOpMonotone.scala
+++ src/main/scala/peal/NonDefaultThLessThanPolOpMonotone.scala
@@ -10,25 +10,22 @@ class NonDefaultThLessThanPolOpMonotone(pol: Pol, th: Double) extends NonDefault
   def enumOne(): Set[Set[Rule]] = {
     val sortedRulesByScore = pol.rules.sortBy(_.score)
     val t = sortedRulesByScore.map(_.score).scanLeft(0.0)((remaining, score) => remaining + score).drop(1)
-
     var m1 = Set[Set[Rule]]()
 
-    def enumOne(x: Set[Rule], index: Integer) {
-      val sum = x.map(_.score).sum
+    def enumOne(x: Set[Rule], sum: Double, index: Integer) {
       if (th < sum) {
         m1 += x
       }
       else {
         var j = index - 1
-        while ((j > 0) && (th < (t(j) + sum))) {
-          enumOne(x + sortedRulesByScore(j), j)
+        while ((j > -1) && (th < (t(j) + sum))) {
+          enumOne(x + sortedRulesByScore(j), sum + sortedRulesByScore(j).score, j)
           j -= 1
         }
       }
     }
 
-    enumOne(Set[Rule](), sortedRulesByScore.size)
+    enumOne(Set[Rule](), 0, sortedRulesByScore.size)
     m1
   }
-
 }
