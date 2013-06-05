382c68b87812fcdd178137d54ebda45d1de12412
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-05 13:46:02 +0100

    changing output to Z3 format

--- src/main/scala/peal/NonDefaultPolLessThanThOpMin.scala
+++ src/main/scala/peal/NonDefaultPolLessThanThOpMin.scala
@@ -4,5 +4,8 @@ import peal.synthesis.NonDefaultSet
 import peal.domain.Pol
 
 class NonDefaultPolLessThanThOpMin(pol: Pol, th: Double) extends NonDefaultSet {
-  def synthesis: String = pol.rules.filter(th > _.score).map(_.q.name).mkString("\n")
+  def synthesis: String = pol.rules.size match {
+    case s if s > 1 => pol.rules.filter(th > _.score).map(_.q.name).mkString("or ", " ", "")
+    case _ => pol.rules.filter(th > _.score).map(_.q.name).mkString(" ")
+  }
 }
--- src/test/scala/peal/NonDefaultPolLessThanThOpMinTest.scala
+++ src/test/scala/peal/NonDefaultPolLessThanThOpMinTest.scala
@@ -10,20 +10,20 @@ class NonDefaultPolLessThanThOpMinTest extends ShouldMatchersForJUnit {
   def testSimpleCaseScoreGreaterThanTh() {
     val p = new Pol(List(new Rule(new Predicate("q1"), 0.7)), 1)
     val pSet = new NonDefaultPolLessThanThOpMin(p, 0.6)
-    pSet.synthesis should be("") // in CNF
+    pSet.synthesis should be("")
   }
 
   @Test
   def testSimpleCaseScoreLessThanTh() {
     val p = new Pol(List(new Rule(new Predicate("q1"), 0.5)), 1)
     val pSet = new NonDefaultPolLessThanThOpMin(p, 0.6)
-    pSet.synthesis should be("q1") // in CNF
+    pSet.synthesis should be("q1")
   }
 
   @Test
   def testSimpleCaseMultipleScoresLessThanTh() {
     val p = new Pol(List(new Rule(new Predicate("q1"), 0.5), new Rule(new Predicate("q2"), 0.2), new Rule(new Predicate("q3"), 0.4)), 1)
     val pSet = new NonDefaultPolLessThanThOpMin(p, 0.6)
-    pSet.synthesis should be("q1\nq2\nq3") // in CNF
+    pSet.synthesis should be("or q1 q2 q3")
   }
 }
