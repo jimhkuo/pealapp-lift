ffdc49c1f86ca3a30bb61b2844a270dcf196a5b4
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-06-09 13:25:01 +0100

    working on ., got antlr to work, added parser test

--- src/test/scala/peal/antlr/ParserTest.scala
+++ src/test/scala/peal/antlr/ParserTest.scala
@@ -6,7 +6,6 @@ import org.scalatest.junit.ShouldMatchersForJUnit
 
 class ParserTest extends ShouldMatchersForJUnit {
 
-
   private def getParser(input: String) = {
     val charStream = new ANTLRStringStream(input)
     val lexer = new PealProgramLexer(charStream)
@@ -16,7 +15,13 @@ class ParserTest extends ShouldMatchersForJUnit {
 
   @Test
   def test() {
-    val parser: PealProgramParser = getParser("cond = pSet <= 0.5\npSet = max(b1, b2)\nb1 = min ((q1 0.2) (q2 0.4) (q3 0.9)) default 1\nb2 = + ((q4 0.1) (q5 0.2) (q6 0.2)) default 0")
+    val input = "cond = pSet <= 0.5\npSet = max(b1, b2)\n" +
+      "b1 = min ((q1 0.2) (q2 0.4) (q3 0.9)) default 1\n" +
+      "b2 = + ((q4 0.1) (q5 0.2) (q6 0.2)) default 0 " +
+      "b3 = + ((q7 0.19) (q8 0.9)) default 0.1 " +
+      "b4 = + ((q4 0.15)) default 0.8 "
+
+    val parser: PealProgramParser = getParser(input)
     parser.program()
 
 
