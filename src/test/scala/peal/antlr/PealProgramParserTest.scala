4ecd0e2a00407b919a158a4d156343c7981d8992
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-06-11 10:04:50 +0100

    set up parser test

--- src/test/scala/peal/antlr/PealProgramParserTest.scala
+++ src/test/scala/peal/antlr/PealProgramParserTest.scala
@@ -3,8 +3,9 @@ package peal.antlr
 import org.junit.Test
 import org.antlr.runtime.{CommonTokenStream, ANTLRStringStream}
 import org.scalatest.junit.ShouldMatchersForJUnit
+import scala.collection.JavaConversions._
 
-class ParserTest extends ShouldMatchersForJUnit {
+class PealProgramParserTest extends ShouldMatchersForJUnit {
 
   private def getParser(input: String) = {
     val charStream = new ANTLRStringStream(input)
@@ -14,19 +15,21 @@ class ParserTest extends ShouldMatchersForJUnit {
   }
 
   @Test
-  def test() {
+  def testCanParseSampleInput() {
     val input = "cond = pSet <= 0.5\npSet = max(b1, b2)\n" +
       "b1 = min ((q1 0.2) (q2 0.4) (q3 0.9)) default 1\n" +
       "b2 = + ((q4 0.1) (q5 0.2) (q6 0.2)) default 0 " +
       "b3 = + ((q7 0.19) (q8 0.9)) default 0.1 " +
       "b4 = + ((q4 0.15)) default 0.8 "
 
-    val parser: PealProgramParser = getParser(input)
-    parser.program()
+    val pealProgrmParser = getParser(input)
+    pealProgrmParser.program()
+    val pols = pealProgrmParser.pols
 
-
-    println(parser.pols)
-
+    pols("b1").rules.size should be(3)
+    pols("b2").rules.size should be(3)
+    pols("b3").rules.size should be(2)
+    pols("b4").rules.size should be(1)
   }
 
 }
