53a6d1f28c581e18d1f29dad63be8f92e1dae9e2
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-21 14:14:48 +0100

    working on NonDefaultThLessThanPolOpMax

---
+++ src/test/scala/peal/NonDefaultThLessThanPolOpMaxTest.scala
@@ -0,0 +1,25 @@
+package peal
+
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Test
+import peal.domain.{Rule, Predicate, Pol}
+import scala.collection.JavaConversions._
+
+
+class NonDefaultThLessThanPolOpMaxTest extends ShouldMatchersForJUnit {
+
+  @Test
+  def testNoScoreGreaterThanTh() {
+    val p = new Pol(List(new Rule(new Predicate("q1"), 0.4)), 1)
+    val pSet = new NonDefaultThLessThanPolOpMax(p, 0.5)
+    pSet.synthesis should be("false")
+  }
+
+  @Test
+  def testOneScoreGreaterThanTh() {
+    val p = new Pol(List(new Rule(new Predicate("q1"), 0.6)), 1)
+    val pSet = new NonDefaultThLessThanPolOpMax(p, 0.5)
+    pSet.synthesis should be("q1")
+  }
+
+}
