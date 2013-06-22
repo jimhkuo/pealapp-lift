0668ae6cfb3ca695cf8151a8b0167f9b1765548e
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-06-22 09:17:24 +0100

    added test place holder

--- src/test/scala/peal/NonDefaultThLessThanPolOpMinTest.scala
+++ src/test/scala/peal/NonDefaultThLessThanPolOpMinTest.scala
@@ -1,10 +1,17 @@
 package peal
 
 import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.{Ignore, Test}
 
 
 class NonDefaultThLessThanPolOpMinTest extends ShouldMatchersForJUnit {
 
   //TODO
   // phi^ndf_min[th < pol] = phi^ndf_antitone[th < pol] = !phi^ndf_min[pol <= th]
+
+  @Ignore("wip")
+  @Test
+  def test() {
+
+  }
 }
