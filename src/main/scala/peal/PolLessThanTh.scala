31f7334e6dbd1a2e98fb40d91f2a4fe3a20a4e6e
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-06-23 19:04:48 +0100

    added notes on bug

--- src/main/scala/peal/PolLessThanTh.scala
+++ src/main/scala/peal/PolLessThanTh.scala
@@ -7,6 +7,7 @@ import scala.collection.JavaConversions._
 
 class PolLessThanTh(pol: Pol, th: Double) extends TopSet {
 
+  //TODO for ndf synthesis it needs to specify an operator
   def synthesis = "(or " + new DefaultPolLessThanTh(pol, th).synthesis + " " + new NonDefaultPolLessThanThOpMin(pol, th).synthesis + ")"
 
   def header: String = pol.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n")
