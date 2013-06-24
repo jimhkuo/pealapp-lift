80e84aed315be20e99fdb2bceba7a0623103b1ab
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-24 10:57:55 +0100

    about to refactor ndf synthesiser to receive operator

--- src/test/scala/peal/NonDefaultPolLessThanThTest.scala
+++ src/test/scala/peal/NonDefaultPolLessThanThTest.scala
@@ -6,7 +6,7 @@ import peal.domain.{Rule, Predicate, Pol}
 import scala.collection.JavaConversions._
 import peal.domain.operator.{Min, Plus}
 
-class NonDefaultPolLessThanThOpMinTest extends ShouldMatchersForJUnit {
+class NonDefaultPolLessThanThTest extends ShouldMatchersForJUnit {
 
   @Test
   def testSimpleCaseScoreGreaterThanTh() {
@@ -72,5 +72,18 @@ class NonDefaultPolLessThanThOpMinTest extends ShouldMatchersForJUnit {
     pSet.synthesis should be("(not (or (and q4 q2 q3) (and q4 q3 q1) (and q5 q2) (and q5 q3) (and q5 q4) (and q5 q1) (and q4 q2 q1)))")
   }
 
+  //TODO M2 computation only works for * operator
+  @Ignore("wip")
+  @Test
+  def testExample3InSynthesisPdf() {
+    val rule5 = new Rule(new Predicate("q5"), 0.1)
+    val rule3 = new Rule(new Predicate("q3"), 0.2)
+    val rule4 = new Rule(new Predicate("q4"), 0.2)
+    val rule2 = new Rule(new Predicate("q2"), 0.3)
+    val rule1 = new Rule(new Predicate("q1"), 0.5)
+    val p = new Pol(List(rule5, rule3, rule4, rule2, rule1), Min, 1)
+    val pSet = new NonDefaultPolLessThanThOpMul(p, 0.25)
 
+    pSet.synthesis should be("(or q5 q4 q3 q2 q1)")
+  }
 }
