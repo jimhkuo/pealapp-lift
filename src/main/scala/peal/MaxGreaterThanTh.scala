3d0fbd1cdbd29283fe2f28b0449f9231e0b9b8d5
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-24 16:48:19 +0100

    done GreaterThanTh

---
+++ src/main/scala/peal/MaxGreaterThanTh.scala
@@ -0,0 +1,14 @@
+package peal
+
+import peal.domain.Pol
+import peal.synthesis.TopSet
+import scala.collection.JavaConversions._
+
+
+class MaxGreaterThanTh(lhs: Pol, rhs: Pol, th: Double) extends TopSet {
+
+  def synthesis = "(and " + new ThLessThanPol(lhs, th).synthesis + " " + new ThLessThanPol(rhs, th).synthesis + ")"
+
+  def z3SMTHeader: String = lhs.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n") +
+    rhs.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n")
+}
