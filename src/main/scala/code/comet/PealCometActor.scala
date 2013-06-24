e3c4a3ecc696673c9c155ae79c99495596f26c06
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-24 12:23:09 +0100

    minor clean up

--- src/main/scala/code/comet/PealCometActor.scala
+++ src/main/scala/code/comet/PealCometActor.scala
@@ -80,9 +80,8 @@ class PealCometActor extends CometActor with Loggable {
     try {
       pealProgrmParser.program()
       val pSet = pealProgrmParser.pSet
-      val result = <pre>
-        {pSet.header}{pSet.notPhi}
-        (get-model)</pre>
+      val result = <pre>{pSet.header}{pSet.notPhi}
+(get-model)</pre>
       this ! Result(result)
     } catch {
       //      case e: RecognitionException => {
