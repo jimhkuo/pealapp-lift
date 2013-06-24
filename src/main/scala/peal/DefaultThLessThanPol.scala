9244e53edbd58fa61be7443eb6782beaf6480ce7
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-24 16:24:08 +0100

    done DefaultThLessThanPol

---
+++ src/main/scala/peal/DefaultThLessThanPol.scala
@@ -0,0 +1,14 @@
+package peal
+
+import peal.synthesis.DefaultSet
+import peal.domain.Pol
+import scala.collection.JavaConversions._
+
+
+class DefaultThLessThanPol(pol: Pol, th: Double) extends DefaultSet {
+  def synthesis: String = pol.defaultScore match {
+    case s if s <= th => "false"
+    case _ if pol.rules.size > 1 => pol.rules.map("(not " + _.q.name + ")").mkString("(and ", " ", ")")
+    case _ => pol.rules.map("(not " + _.q.name + ")").mkString(" ")
+  }
+}
