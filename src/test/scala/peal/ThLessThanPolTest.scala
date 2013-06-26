639782a1d53c4c1421d0db55f4fcd4192b6a95a4
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-06-26 09:38:58 +0100

    going to work on ThLessThanPolTest

--- src/test/scala/peal/ThLessThanPolTest.scala
+++ src/test/scala/peal/ThLessThanPolTest.scala
@@ -9,16 +9,16 @@ import peal.domain.operator.Min
 class ThLessThanPolTest extends ShouldMatchersForJUnit {
 
   @Test
-  def testSimpleCaseScoreLessThanThDifferentDefault() {
-    val p = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 0)
-    val phi = new ThLessThanPol(p, 0.6)
-    phi.synthesis should be("(or false (not q1))")
-  }
-
-  @Test
-  def testCanSynthesisSimpleCase() {
+  def testThLessThanDefault() {
     val p = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 0.7)
     val phi = new ThLessThanPol(p, 0.6)
     phi.synthesis should be("(or (not q1) (not q1))")
+  }
+
+  @Test
+  def testDefaultLessThanTh() {
+    val p = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 0)
+    val phi = new ThLessThanPol(p, 0.6)
+    phi.synthesis should be("(or false (not q1))")
   }
 }
