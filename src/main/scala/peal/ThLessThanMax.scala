b2a026ce7ce302fa259dccf48cece0e861ac4871
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-06-26 09:52:33 +0100

    fixed bug in ThLessThanMax

--- src/main/scala/peal/ThLessThanMax.scala
+++ src/main/scala/peal/ThLessThanMax.scala
@@ -7,7 +7,7 @@ import scala.collection.JavaConversions._
 
 class ThLessThanMax(lhs: Pol, rhs: Pol, th: Double) extends TopSet {
 
-  def synthesis = "(and " + new ThLessThanPol(lhs, th).synthesis + " " + new ThLessThanPol(rhs, th).synthesis + ")"
+  def synthesis = "(or " + new ThLessThanPol(lhs, th).synthesis + " " + new ThLessThanPol(rhs, th).synthesis + ")"
 
   def z3SMTHeader: String = lhs.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n") +
     rhs.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n")
--- src/test/scala/peal/ThLessThanMaxTest.scala
+++ src/test/scala/peal/ThLessThanMaxTest.scala
@@ -13,7 +13,7 @@ class ThLessThanMaxTest extends ShouldMatchersForJUnit {
     val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
     val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
     val phi = new ThLessThanMax(p1, p2, 0.6)
-
-    phi.synthesis should be("(and (or (not q1) (not q1)) (or false (not q2)))")
+    println(phi.synthesis)
+    phi.synthesis should be("(or (or (not q1) (not q1)) (and q2 (not q2)))")
   }
 }
