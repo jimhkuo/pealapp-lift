2e94def86f807c480ed7c7f29e986c5863d38349
hk2109 <jimhkuo@imperial.ac.uk>
2013-06-27 16:14:50 +0100

    added ScalaZ3 test examples

---
+++ src/test/scala/z3/ScalaZ3Test.scala
@@ -0,0 +1,65 @@
+package z3
+
+import org.junit.Test
+import org.scalatest.junit.ShouldMatchersForJUnit
+import z3.scala.dsl._
+import z3.scala.dsl.Operands._
+import z3.scala._
+
+class ScalaZ3Test extends ShouldMatchersForJUnit {
+
+  @Test
+  def testCalendar() {
+    val totalDays = 10593
+    val originYear = 1980
+
+    val (year, day) = choose((year: Val[Int], day: Val[Int]) => {
+      def leapDaysUntil(y : Tree[IntSort]) = (y - 1) / 4 - (y - 1) / 100 + (y - 1) / 400
+
+      totalDays === (year - originYear) * 365 + leapDaysUntil(year) - leapDaysUntil(originYear) + day &&
+        day > 0 && day <= 366
+    })
+
+    year should equal (2008)
+    day should equal (366)
+  }
+
+  @Test
+  def testComfusyLike() {
+    val z3 = new Z3Context(new Z3Config("MODEL" -> true))
+    val i = z3.mkIntSort
+    val h = z3.mkConst(z3.mkStringSymbol("h"), i)
+    val m = z3.mkConst(z3.mkStringSymbol("m"), i)
+    val s = z3.mkConst(z3.mkStringSymbol("s"), i)
+    // builds a constant integer value from the CL arg.
+    val t = z3.mkInt(1234, i)
+    // more integer constants
+    val z = z3.mkInt(0, i)
+    val sx = z3.mkInt(60, i)
+    // builds the constraint h*3600 + m * 60 + s == totSecs
+    val cs1 = z3.mkEq(
+      z3.mkAdd(
+        z3.mkMul(z3.mkInt(3600, i), h),
+        z3.mkMul(sx,                m),
+        s),
+      t)
+    // more constraints
+    val cs2 = z3.mkAnd(z3.mkGE(h, z), z3.mkLT(h, z3.mkInt(24, i)))
+    val cs3 = z3.mkAnd(z3.mkGE(m, z), z3.mkLT(m, sx))
+    val cs4 = z3.mkAnd(z3.mkGE(s, z), z3.mkLT(s, sx))
+
+    val solver = z3.mkSolver
+    solver.assertCnstr(z3.mkAnd(cs1, cs2, cs3, cs4))
+
+    // attempting to solve the constraints
+    val (sol, model) = solver.checkAndGetModel
+
+    sol should equal(Some(true))
+    model.evalAs[Int](h) should equal(Some(0))
+    model.evalAs[Int](m) should equal(Some(20))
+    model.evalAs[Int](s) should equal(Some(34))
+
+    model.delete
+    z3.delete
+  }
+}
