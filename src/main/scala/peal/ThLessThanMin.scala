2d0c62562b8b759968007a88cc56a42cf6259909
hk2109 <jimhkuo@imperial.ac.uk>
2013-07-03 15:55:23 +0100

    can deal with min when th < pol

--- src/main/scala/peal/ThLessThanMin.scala
+++ src/main/scala/peal/ThLessThanMin.scala
@@ -5,10 +5,13 @@ import peal.synthesis.pSet
 import scala.collection.JavaConversions._
 
 
-class ThLessThanMin(lhs: Pol, rhs: Pol, th: Double) extends pSet {
+class ThLessThanMin(lhs: Pol, rhs: pSet, th: Double) extends pSet {
 
-  def synthesis = "(and " + new ThLessThanPol(lhs, th).synthesis + " " + new ThLessThanPol(rhs, th).synthesis + ")"
+  def synthesis = rhs match {
+    case _ : Pol => "(and " + new ThLessThanPol(lhs, th).synthesis + " " + new ThLessThanPol(rhs.getPol, th).synthesis + ")"
+    case _ => "(and " + new ThLessThanPol(lhs, th).synthesis + " " + rhs.synthesis + ")"
+  }
 
   def z3SMTHeader: String = lhs.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n") +
-    rhs.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n")
+    rhs.getPol.rules.map(p => "(declare-const " + p.q.name + " Bool)").mkString("", "\n", "\n")
 }
--- src/test/scala/peal/ThLessThanMinTest.scala
+++ src/test/scala/peal/ThLessThanMinTest.scala
@@ -9,6 +9,15 @@ import peal.domain.operator.Min
 class ThLessThanMinTest extends ShouldMatchersForJUnit {
 
   @Test
+  def testCanSynthesisSimpleNestedCase() {
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
+    val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
+    val phi1 = new ThLessThanPol(p2, 0.6)
+    val phi2 = new ThLessThanMin(p1, phi1, 0.6)
+    phi2.synthesis should be("(and (or (not q1) (not q1)) (and q2 (not q2)))")
+  }
+
+  @Test
   def testCanSynthesisSimpleCase() {
     val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
     val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
