49c5222ef08ca4c28bc27e3a08aac7b6bbee0d9b
hk2109 <jimhkuo@imperial.ac.uk>
2013-07-03 15:07:40 +0100

    got nested to work

--- src/main/scala/peal/MaxLessThanTh.scala
+++ src/main/scala/peal/MaxLessThanTh.scala
@@ -10,6 +10,7 @@ class MaxLessThanTh(lhs: Pol, rhs: pSet, th: Double) extends pSet {
 
   def synthesis = rhs match {
     case _: Pol => "(and " + new PolLessThanTh(lhs, th).synthesis + " " + new PolLessThanTh(rhs.getPol, th).synthesis + ")"
+    case _ => "(and " + new PolLessThanTh(lhs, th).synthesis + " " + rhs.synthesis + ")"
   }
 
 
--- src/test/scala/peal/MaxLessThanThTest.scala
+++ src/test/scala/peal/MaxLessThanThTest.scala
@@ -9,6 +9,26 @@ import peal.domain.operator.Min
 class MaxLessThanThTest extends ShouldMatchersForJUnit {
 
   @Test
+  def testCanSynthesisNestedMoreCase() {
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
+    val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
+    val phi1 = new MaxLessThanTh(p1, p2, 0.6)
+    val phi2 = new MaxLessThanTh(p1, phi1, 0.6)
+    phi2.synthesis should be("(and (and q1 q1) (and (and q1 q1) (or (not q2) q2)))")
+  }
+
+  @Test
+  def testCanSynthesisNestedCase() {
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
+    val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
+    val phi1 = new PolLessThanTh(p2, 0.6)
+    val phi2 = new MaxLessThanTh(p1, phi1, 0.6)
+
+//    println(phi2.synthesis)
+    phi2.synthesis should be("(and (and q1 q1) (or (not q2) q2))")
+  }
+
+  @Test
   def testCanSynthesisSimpleCase() {
     val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
     val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
