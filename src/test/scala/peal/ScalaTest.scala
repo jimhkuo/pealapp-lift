4ea181a811700b46d0c203345b60f0058ee8e1c0
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-04 14:58:34 +0100

    added sample to clarify scala

--- src/test/scala/peal/ScalaTest.scala
+++ src/test/scala/peal/ScalaTest.scala
@@ -10,14 +10,26 @@ import scala.collection.JavaConversions._
 class ScalaTest extends ShouldMatchersForJUnit {
 
   @Test
-  def testFor() {
+  def testForMap() {
 
     val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5), new Rule(new Predicate("q2"), 0.5), new Rule(new Predicate("q3"), 0.5)), Min, 1)
     val p2 = new Pol(List(new Rule(new Predicate("q4"), 0.5), new Rule(new Predicate("q5"), 0.5), new Rule(new Predicate("q6"), 0.5)), Min, 0)
     val pols = Map("1" -> p1, "2" -> p2)
 
-    val q = pols.values.flatMap(pol => pol.rules).map(r => r.q.name).toSeq.distinct
+    val rules = pols.values.map(pol => pol.rules)
+    println(rules)
+    val flatRules = pols.values.flatMap(pol => pol.rules)
+    println(flatRules)
+    val q = flatRules.map(r => r.q.name).toSeq.distinct
     println(q)
+  }
+
+  @Test
+  def testFor() {
+
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5), new Rule(new Predicate("q2"), 0.5), new Rule(new Predicate("q3"), 0.5)), Min, 1)
+    val p2 = new Pol(List(new Rule(new Predicate("q4"), 0.5), new Rule(new Predicate("q5"), 0.5), new Rule(new Predicate("q6"), 0.5)), Min, 0)
+    val pols = Map("1" -> p1, "2" -> p2)
 
     val r = for (
       pol <- pols.values;
