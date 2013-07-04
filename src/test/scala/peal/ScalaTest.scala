0df0736fa27e959d49d4bb4caed82c6e0eb915b5
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-04 14:44:48 +0100

    added test case to demonstrate confusion

---
+++ src/test/scala/peal/ScalaTest.scala
@@ -0,0 +1,34 @@
+package peal
+
+import org.junit.Test
+import peal.domain.{Predicate, Rule, Pol}
+import peal.domain.operator.Min
+import org.scalatest.junit.ShouldMatchersForJUnit
+import scala.collection.JavaConversions._
+
+
+class ScalaTest extends ShouldMatchersForJUnit {
+
+  @Test
+  def testFor() {
+
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5), new Rule(new Predicate("q2"), 0.5), new Rule(new Predicate("q3"), 0.5)), Min, 1)
+    val p2 = new Pol(List(new Rule(new Predicate("q4"), 0.5), new Rule(new Predicate("q5"), 0.5), new Rule(new Predicate("q6"), 0.5)), Min, 0)
+
+    val pols = Map("1" -> p1, "2" -> p2)
+
+    val q = pols.values.flatMap(pol => pol.rules.toList).map(r => r.q.name).toSeq.distinct
+    val s = for (name <- q) yield name
+    println(s)
+
+    val r = for (
+      pol <- pols.values;
+      rules <- pol.rules;
+      name <- rules.q.name
+    ) yield name
+
+    println(r)
+
+  }
+
+}
