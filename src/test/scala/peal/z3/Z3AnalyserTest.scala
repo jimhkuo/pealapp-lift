b1d2da23f4823d52bf578784e984820aab010a43
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-05 10:25:49 +0100

    added place holder test for first analysis

--- src/test/scala/peal/z3/Z3AnalyserTest.scala
+++ src/test/scala/peal/z3/Z3AnalyserTest.scala
@@ -8,12 +8,20 @@ import org.junit.Test
 class Z3AnalyserTest extends ShouldMatchersForJUnit {
 
   @Test
-  def testCanRunSimpleFirstAnalysis() {
+  def testFirstAnalysisNotAlwaysTrue() {
 
     val input = "cond = pSet <= 0.5" +
       "b1 = min ((q1 0.2) (q2 0.4) (q3 0.9)) default 1" +
       "b2 = + ((q4 0.1) (q5 0.2) (q6 0.2)) default 0 " +
       "pSet = max(b1, b2)"
+
+    new Z3Analyser().isAlwaysTrue(input) should be (false)
+  }
+
+  @Test
+  def testFirstAnalysisAlwaysFalse() {
+
+    val input = "cond = pSet <= 0.5\nb1 = + ((q1 0.9)) default 1\npSet = b1"
 
     new Z3Analyser().isAlwaysTrue(input) should be (false)
   }
