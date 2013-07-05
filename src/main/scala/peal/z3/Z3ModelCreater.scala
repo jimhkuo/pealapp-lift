b24aca422c8aea33758180efaf34c62629d638ca
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-05 09:46:48 +0100

    created place holder for Z3 Model Creator

---
+++ src/main/scala/peal/z3/Z3ModelCreater.scala
@@ -0,0 +1,5 @@
+package peal.z3
+
+object Z3ModelCreater {
+
+}
---
+++ src/test/scala/peal/z3/Z3ModelCreaterTest.scala
@@ -0,0 +1,13 @@
+package peal.z3
+
+import z3.scala.dsl._
+import z3.scala._
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Test
+
+class Z3ModelCreaterTest extends ShouldMatchersForJUnit {
+
+  @Test
+  def testCreateSimpleModel() {
+  }
+}
