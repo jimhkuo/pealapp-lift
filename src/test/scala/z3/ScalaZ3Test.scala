22c125eb3213cfcffdbc242269239ae0014b6ebf
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-06 16:02:00 +0100

    testing Z3

--- src/test/scala/z3/ScalaZ3Test.scala
+++ src/test/scala/z3/ScalaZ3Test.scala
@@ -7,6 +7,50 @@ import z3.scala._
 
 class ScalaZ3Test extends ShouldMatchersForJUnit {
 
+
+  @Test
+  def testMxingContext() {
+    var z3 = new Z3Context(new Z3Config("MODEL" -> true))
+    val q1a = z3.mkBoolConst("q1")
+    val f = z3.mkFalse()
+    val solver = z3.mkSolver
+    solver.assertCnstr(z3.mkAnd(q1a, f))
+
+    var (sol, model) = solver.checkAndGetModel
+
+    sol should be (Some(false))
+
+    if (sol.get == true) {
+      model.delete
+    }
+
+    val q1 = z3.mkBoolConst("q1")
+    val q2 = z3.mkBoolConst("q2")
+    val q1Orq2 = z3.mkOr(q1, q2)
+    val formula = z3.mkAnd(q1Orq2, q1)
+    println(formula)
+    //function that takes no arguments is a constant
+    val cond = z3.mkBoolConst("cond")
+    val equal = z3.mkEq(cond, formula)
+    println(equal)
+    val solver1 = z3.mkSolver
+    solver1.assertCnstr(equal)
+    solver1.assertCnstr(z3.mkNot(cond))
+
+    var (sol1, model1) = solver1.checkAndGetModel
+
+    sol1 should be(Some(true))
+    model1.evalAs[Boolean](q1) should be(Some(false))
+
+    if (sol1.get == true) {
+      model1.delete
+    }
+
+    z3.delete()
+    z3 = null
+  }
+
+
   @Test
   def testFunction2() {
     //    (declare-const x Real)
@@ -30,6 +74,7 @@ class ScalaZ3Test extends ShouldMatchersForJUnit {
     model.evalAs[Int](x) should be(Some(0))
     model.evalAs[Int](y) should be(Some(-1))
 
+
     model.delete
     z3.delete()
     z3 = null
@@ -52,7 +97,7 @@ class ScalaZ3Test extends ShouldMatchersForJUnit {
     //function that takes no arguments is a constant
     val cond = z3.mkBoolConst("cond")
     val equal = z3.mkEq(cond, formula)
-    println (equal)
+    println(equal)
     val solver = z3.mkSolver
     solver.assertCnstr(equal)
     solver.assertCnstr(z3.mkNot(cond))
