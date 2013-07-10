775e96de3b529dbca9824aeb27982f882c7554d7
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-10 16:09:05 +0100

    added notes

--- src/main/scala/peal/synthesis/pSet.scala
+++ src/main/scala/peal/synthesis/pSet.scala
@@ -3,7 +3,9 @@ package peal.synthesis
 import peal.domain.Pol
 import z3.scala.{Z3AST, Z3Context}
 
+//TODO this should probably be renamed to Condition
 trait pSet {
+  //TODO synthesis should go with cond and only triggered in analysis
 
   def getPol : Pol = null
 
