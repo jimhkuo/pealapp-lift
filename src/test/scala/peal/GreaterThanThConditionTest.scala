06e273d08592717a5a9ea00a95d68ca5244d3b9e
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-10 17:15:21 +0100

    done testCanSynthesisNestedMoreCaseForMax

--- src/test/scala/peal/GreaterThanThConditionTest.scala
+++ src/test/scala/peal/GreaterThanThConditionTest.scala
@@ -43,9 +43,9 @@ class GreaterThanThConditionTest extends ShouldMatchersForJUnit with Z3ModelMatc
   def testCanSynthesisNestedMoreCaseForMax() {
     val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
     val p2 = new Pol(List(new Rule(new Predicate("q2"), 0.5)), Min, 0)
-    val pSet1 = new MaxPolicySet(new PolPolicySet(p1), new PolPolicySet(p2))
+    val pSet1 = new PolPolicySet(p2)
     val pSet2 = new MaxPolicySet(new PolPolicySet(p1), pSet1)
-    val phi = new LessThanThCondition(pSet2, 0.6)
-    phi.synthesis(z3, consts) should beZ3Model("(and (and q1 q1) (and (and q1 q1) (or (not q2) q2)))")
+    val phi = new GreaterThanThCondition(pSet2, 0.6)
+    phi.synthesis(z3, consts) should beZ3Model("(or (or (not q1) (not q1)) (and q2 (not q2)))")
   }
 }
