491002afd3cf8f224f99016691368821370f8de1
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-10 18:40:06 +0100

    clean up unused code

--- src/main/scala/peal/synthesis/Condition.scala
+++ src/main/scala/peal/synthesis/Condition.scala
@@ -3,8 +3,5 @@ package peal.synthesis
 import z3.scala.{Z3AST, Z3Context}
 
 trait Condition {
-
   def synthesis(z3: Z3Context, consts: Map[String, Z3AST]): Z3AST
-
-  def phiZ3SMTString(z3: Z3Context, consts: Map[String, Z3AST]) = "(assert " + synthesis(z3, consts) + ")"
 }
