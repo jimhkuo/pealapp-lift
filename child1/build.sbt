7a88b325aaa2bad38075f0887baa0c69aca9e67b
hk2109 <jimhkuo@imperial.ac.uk>
2013-07-24 15:33:11 +0100

    add child1

---
+++ child1/build.sbt
@@ -0,0 +1,6 @@
+name := "child1"
+
+version := "0.1"
+
+scalaVersion := "2.10.1"
+
--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import sbt._
 import Keys._
 
 object PealBuild extends Build {
-  lazy val root = Project(id = "PealApp-lift", base = file(".")) dependsOn  (pmg)
+  lazy val root = Project(id = "PealApp-lift", base = file(".")) dependsOn (pmg)
 
   lazy val pmg = Project(id = "child1", base = file("child1"))
 }
