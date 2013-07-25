cd975d719709c804f6a63e3852e21018da32836a
hk2109 <jimhkuo@imperial.ac.uk>
2013-07-25 11:35:48 +0100

    can deal with all four operators

--- PealModelGenerator/src/test/scala/peal/domain/PolTest.scala
+++ PealModelGenerator/src/test/scala/peal/domain/PolTest.scala
@@ -2,7 +2,7 @@ package peal.domain
 
 import org.scalatest.junit.ShouldMatchersForJUnit
 import org.junit.Test
-import peal.domain.operator.Min
+import peal.domain.operator._
 import scala.collection.JavaConversions._
 
 
@@ -10,9 +10,10 @@ class PolTest extends ShouldMatchersForJUnit {
 
   @Test
   def testCanOutput() {
-    val p1 = new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1)
-
-    p1.toString should be("min ((q1 0.5)) default 1.0")
+    new Pol(List(new Rule(new Predicate("q1"), 0.5)), Min, 1).toString should be("min ((q1 0.5)) default 1.0")
+    new Pol(List(new Rule(new Predicate("q2"), 0.1)), Max, 0.5).toString should be("max ((q2 0.1)) default 0.5")
+    new Pol(List(new Rule(new Predicate("q3"), 0.5)), Plus, 1).toString should be("+ ((q3 0.5)) default 1.0")
+    new Pol(List(new Rule(new Predicate("q4"), 0.5)), Mul, 1).toString should be("* ((q4 0.5)) default 1.0")
   }
 
 }
--- src/main/scala/peal/domain/operator/Operators.scala
+++ src/main/scala/peal/domain/operator/Operators.scala
@@ -6,10 +6,14 @@ object Plus extends Operators {
   override def toString = "+"
 }
 
-object Mul extends Operators
+object Mul extends Operators {
+  override def toString = "*"
+}
 
 object Min extends Operators {
   override def toString = "min"
 }
 
-object Max extends Operators
+object Max extends Operators {
+  override def toString = "max"
+}
