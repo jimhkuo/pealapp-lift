8c584aefa721d96d11af71b8c649ce643ed9e2b9
hk2109 <jimhkuo@imperial.ac.uk>
2013-07-30 11:21:03 +0100

    set up ExperimentRunner

--- project/PealBuild.scala
+++ project/PealBuild.scala
@@ -5,10 +5,6 @@ import Keys._
 
 object PealBuild extends Build {
 
-  def standardSettings = Seq(
-    exportJars := true
-  ) ++ Defaults.defaultSettings
-
   lazy val root = Project(id = "PealApp-lift", base = file(".")) aggregate (domain, pmg) dependsOn(domain, pmg)
 
   lazy val runner = Project(id = "ExperimentRunner", base = file("ExperimentRunner")) dependsOn(pmg)
