978c2d7f160b7f46019f396ec1950405c88cec90
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-07-31 08:50:11 +0100

    set up output

--- ExperimentRunner/src/main/scala/peal/runner/ExperimentRunner.scala
+++ ExperimentRunner/src/main/scala/peal/runner/ExperimentRunner.scala
@@ -21,31 +21,40 @@ class ExperimentRunner(z3: Z3Context, duration: Long) {
       implicit val timeout = Timeout(duration, MILLISECONDS)
 
       val generatorRunner = system.actorOf(Props(new ModelGeneratorActor(params)))
+      var start = System.nanoTime()
       val modelFuture = generatorRunner ? Run
       val model = Await.result(modelFuture, timeout.duration).asInstanceOf[String]
+      var lapsedTime = System.nanoTime() - start
+      print(lapsedTime + ",")
 
       try {
         val z3InputGenerator = system.actorOf(Props(new Z3InputGeneratorActor(z3)))
+        start = System.nanoTime()
         val inputFuture = z3InputGenerator ? model
         val input = Await.result(inputFuture, timeout.duration)
+        lapsedTime = System.nanoTime() - start
+        print(lapsedTime + ",")
 
         try {
           val z3Caller = system.actorOf(Props(new Z3CallerActor(z3Path)))
+          start = System.nanoTime()
           val resultFuture = z3Caller ? input
           val result = Await.result(resultFuture, timeout.duration)
+          lapsedTime = System.nanoTime() - start
+          print(lapsedTime)
 
-          println("result:\n" + result)
+//          println("result:\n" + result)
         }
         catch {
           case e: TimeoutException => println("timed out in z3")
         }
       }
       catch {
-        case e: TimeoutException => println("timed out in synthesis")
+        case e: TimeoutException => println("timed out in synthesis,NotRun")
       }
     }
     catch {
-      case e: TimeoutException => println("timed out in model generation")
+      case e: TimeoutException => println("timed out in model generation,NotRun,NotRun")
     }
     finally {
       system.shutdown()
