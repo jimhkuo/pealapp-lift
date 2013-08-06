6ed6e0742d91a0ddb35048d9356ce251df4ef0ba
hk2109 <jimhkuo@imperial.ac.uk>
2013-08-06 11:55:32 +0100

    hooked up genMin/Max

--- src/test/scala/peal/synthesis/LazySynthesisSpike.scala
+++ src/test/scala/peal/synthesis/LazySynthesisSpike.scala
@@ -105,8 +105,17 @@ class LazySynthesisSpike extends ShouldMatchersForJUnit {
 //            "(or (and (<= " + pols(bName).defaultScore + " " + cond.getTh + ") (not (or " + pols(bName).rules.map(_.q.name).mkString(" ") + ")))\n" +
 //              "(and (or " + pols(bName).rules.map(_.q.name).mkString(" ") + ") " + "(not (or " + pols(bName).rules.filter(_.score <= cond.getTh).map(_.q.name).mkString(" ") + "))))))"
           case cond: GreaterThanThCondition =>
-            "(or (and (< " + cond.getTh + " " + pols(bName).defaultScore + ") (not (or " + pols(bName).rules.map(_.q.name).mkString(" ") + ")))\n" +
-              "(or " + pols(bName).rules.filter(cond.getTh < _.score).map(_.q.name).mkString(" ") + "))))"
+            val filtered = pols(bName).rules.filter(cond.getTh < _.score)
+            if (filtered.size > 0) {
+              "(or (and (< " + cond.getTh + " " + pols(bName).defaultScore + ") (not (or " + pols(bName).rules.map(_.q.name).mkString(" ") + ")))\n" +
+                "(or " + filtered.map(_.q.name).mkString(" ") + ")))"
+            }
+            else {
+              "(or (and (< " + cond.getTh + " " + pols(bName).defaultScore + ") (not (or " + pols(bName).rules.map(_.q.name).mkString(" ") + "))))\n"
+            }
+
+//            "(or (and (< " + cond.getTh + " " + pols(bName).defaultScore + ") (not (or " + pols(bName).rules.map(_.q.name).mkString(" ") + ")))\n" +
+//              "(or " + pols(bName).rules.filter(cond.getTh < _.score).map(_.q.name).mkString(" ") + "))))"
         }
         println("(assert (= " + condName + "_" + bName + " " + genFormula + ")")
       case o =>
