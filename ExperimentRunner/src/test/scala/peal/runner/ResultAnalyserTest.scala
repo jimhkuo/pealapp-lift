6a191d10e43e7e262e8bb64a1c29865441a35a6d
hk2109 <jimhkuo@imperial.ac.uk>
2013-08-09 15:02:17 +0100

    building result anaylyser

--- ExperimentRunner/src/test/scala/peal/runner/ResultAnalyserTest.scala
+++ ExperimentRunner/src/test/scala/peal/runner/ResultAnalyserTest.scala
@@ -23,8 +23,8 @@ class ResultAnalyserTest extends ShouldMatchersForJUnit {
 
   @Test
   def testResultAnalyser() {
-    val input = "Result of analysis [analysis1 = always_true? cond1]:\n\nsat\n\n(model \n\n  (define-fun cond1 () Bool\n\n    false)\n\n  (define-fun q5 () Bool\n\n    false)\n\n  (define-fun q0 () Bool\n\n    false)\n\n  (define-fun always_true_analysis1 () Bool\n\n    false)"
+    val input = "Result of analysis [analysis1 = always_true? cond1]:\n\nsat\n\n(model \n\n  (define-fun cond1 () Bool\n\nResult of analysis [analysis2 = always_false? cond2]:\n\nunsat\n\n(error \"line 199 column 10: model is not available\")\n\nResult of analysis [analysis3 = different? cond1 cond2]:\n\nsat\n\n(model "
     val results = ResultAnalyser.execute(input)
-    results should be(Map("analysis1 = always_true? cond1" -> "sat"))
+    results should be(Map("analysis1 = always_true? cond1" -> "sat", "analysis2 = always_false? cond2" -> "unsat", "analysis3 = different? cond1 cond2" -> "sat"))
   }
 }
