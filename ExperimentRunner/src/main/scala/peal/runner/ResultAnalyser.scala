1c78b33a7292c974e45b53289fbb602bee48c231
hk2109 <jimhkuo@imperial.ac.uk>
2013-08-09 14:52:23 +0100

    added result analyser test

--- ExperimentRunner/src/main/scala/peal/runner/ResultAnalyser.scala
+++ ExperimentRunner/src/main/scala/peal/runner/ResultAnalyser.scala
@@ -2,4 +2,19 @@ package peal.runner
 
 object ResultAnalyser {
 
+  val spec = """Result of analysis \[([-\w. =?]+)\]:""".r
+
+  def execute(input: String) : Map[String, String] = {
+
+    input.split("\n").foreach {
+      line =>
+           line match {
+             case spec(name) =>
+             case _ =>
+           }
+    }
+
+    Map()
+  }
+
 }
--- ExperimentRunner/src/test/scala/peal/runner/ResultAnalyserTest.scala
+++ ExperimentRunner/src/test/scala/peal/runner/ResultAnalyserTest.scala
@@ -4,18 +4,29 @@ import org.scalatest.junit.ShouldMatchersForJUnit
 import org.junit.Test
 
 class ResultAnalyserTest extends ShouldMatchersForJUnit {
-
-//  val spec = """([-\w]+) ([-\d ]+)[,]G[,](?s).*[,]W0 [(]([-\d]+) nodes[)]:[,]W1 [(]([-\d]+) nodes[)]:[,]([-\d.]+)(?s).*[,]P(?s).*[,]([-\w.]+)[,](?s).*""".r
   val spec = """Result of analysis \[([-\w. =?]+)\]:""".r
+  val status = """[ ]+([-\w]+)[ ]+""".r
 
   @Test
   def testCanDealWithZ3Model() {
-//    val input = "Result of analysis [analysis1 = always_true? cond1]:\n\nsat\n\n(model \n\n  (define-fun cond1 () Bool\n\n    false)\n\n  (define-fun q5 () Bool\n\n    false)\n\n  (define-fun q0 () Bool\n\n    false)\n\n  (define-fun always_true_analysis1 () Bool\n\n    false)"
     val input = "Result of analysis [analysis1 = always_true? cond1]:"
-
     val spec(name) = input
     println(name)
+    name should be("analysis1 = always_true? cond1")
+  }
 
-    name should be ("analysis1 = always_true? cond1")
+  @Test
+  def testStatus() {
+    val status(s) = "  sat  "
+    s should be ("sat")
+    val status(us) = "  unsat  "
+    us should be ("unsat")
+  }
+
+  @Test
+  def testResultAnalyser() {
+    val input = "Result of analysis [analysis1 = always_true? cond1]:\n\nsat\n\n(model \n\n  (define-fun cond1 () Bool\n\n    false)\n\n  (define-fun q5 () Bool\n\n    false)\n\n  (define-fun q0 () Bool\n\n    false)\n\n  (define-fun always_true_analysis1 () Bool\n\n    false)"
+    val results = ResultAnalyser.execute(input)
+    results should be(Map("analysis1 = always_true? cond1" -> "sat"))
   }
 }
