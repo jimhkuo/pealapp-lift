f2a5458942fadaa61480c43a3138120d46784082
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-08-10 07:49:05 +0100

    set up new default experiment

--- ExperimentRunner/src/main/scala/peal/runner/Main.scala
+++ ExperimentRunner/src/main/scala/peal/runner/Main.scala
@@ -6,7 +6,7 @@ import z3.scala.{Z3Config, Z3Context}
 object Main extends App {
   val z3 = new Z3Context(new Z3Config("MODEL" -> true))
   try {
-    new ExperimentRunner(z3, 1000).run("100, 5, 5, 5, 5, 7, 0.6, 0.1", "/Users/jkuo/tools/z3/bin")
+    new ExperimentRunner(z3, 2000).run("100, 5, 5, 5, 5, 7, 0.6, 0.1", "/Users/jkuo/tools/z3/bin")
   } finally {
     z3.delete
   }
