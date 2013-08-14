b3b6c8d24290c825be90822bfed5ffcbc4254cd6
hk2109 <jimhkuo@imperial.ac.uk>
2013-08-14 09:54:36 +0100

    set up better reporting

--- ExperimentRunner/src/main/scala/peal/runner/Main.scala
+++ ExperimentRunner/src/main/scala/peal/runner/Main.scala
@@ -37,18 +37,34 @@ object Main extends App {
   private def execute(n: Int, m0: Int, m1: Int, m2: Int, m3: Int, th: Double, delta: Double): Boolean = {
     var z3: Z3Context = null
     val z3Path: String = if (args.size == 0) "/Users/jkuo/tools/z3/bin" else args(0)
-    try {
+    var mt = 0l
+    var et = 0l
+    var ezt = 0l
+    var lt = 0l
+    var lzt = 0l
 
+    try {
       for (i <- 1 to 5) {
         z3 = new Z3Context(new Z3Config("MODEL" -> true))
-        val output = new ExperimentRunner(300000).run(n, m0, m1, m2, m3, 3 * p, th, delta, z3Path)
-        println(n + "-" + m0 + "-" + m1 + "-" + m2 + "-" + m3 + "-" + th + "-" + delta + "," + milliTime(output.modelGeneration) + "," + milliTime(output.eagerSynthesis) + "," + milliTime(output.eagerZ3) + "," + milliTime(output.lazySynthesis) + "," + milliTime(output.lazyZ3) + "," + output.isSameOutput.toString.toUpperCase + "," + output.model1Result + "," + output.pealInput)
+        val output = new ExperimentRunner(5000).run(n, m0, m1, m2, m3, 3 * p, th, delta, z3Path)
+        mt += output.modelGeneration
+        et += output.eagerSynthesis
+        ezt += output.eagerZ3
+        lt += output.lazySynthesis
+        lzt += output.lazyZ3
+        //        println(n + "-" + m0 + "-" + m1 + "-" + m2 + "-" + m3 + "-" + th + "-" + delta + "," + milliTime(output.modelGeneration) + "," + milliTime(output.eagerSynthesis) + "," + milliTime(output.eagerZ3) + "," + milliTime(output.lazySynthesis) + "," + milliTime(output.lazyZ3) + "," + output.isSameOutput.toString.toUpperCase + "," + output.model1Result + "," + output.pealInput)
+        if (!output.isSameOutput) {
+          println("eager and lazy produce different result," + n + "-" + m0 + "-" + m1 + "-" + m2 + "-" + m3 + "-" + th + "-" + delta + "," + output.pealInput)
+        }
         z3.delete
         z3 = null
       }
+
+      println("," + n + "-" + m0 + "-" + m1 + "-" + m2 + "-" + m3 + "-" + th + "-" + delta + "," + milliTime(mt / 5) + "," + milliTime(et / 5) + "," + milliTime(ezt / 5) + "," + milliTime(lt / 5) + "," + milliTime(lzt / 5))
       true
     } catch {
       case e: TimeoutException =>
+        println("," + n + "-" + m0 + "-" + m1 + "-" + m2 + "-" + m3 + "-" + th + "-" + delta + ",-1,-1,-1,-1,-1")
         false
     }
     finally {
