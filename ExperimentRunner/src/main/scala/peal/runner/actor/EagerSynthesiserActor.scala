5b29d0673879a3b2fc8df3e05d66380ca08a2742
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-08-22 11:42:02 +0200

    able to clean up

--- ExperimentRunner/src/main/scala/peal/runner/actor/EagerSynthesiserActor.scala
+++ ExperimentRunner/src/main/scala/peal/runner/actor/EagerSynthesiserActor.scala
@@ -1,6 +1,6 @@
 package peal.runner.actor
 
-import akka.actor.{ReceiveTimeout, Actor}
+import akka.actor.Actor
 import org.antlr.runtime.{CommonTokenStream, ANTLRStringStream}
 import peal.antlr.{PealProgramParser, PealProgramLexer}
 import z3.scala.{Z3Config, Z3Context}
@@ -10,7 +10,8 @@ import peal.runner.TimedOut
 
 class EagerSynthesiserActor extends Actor {
 
-  val z3 = new Z3Context(new Z3Config("MODEL" -> true))
+  var z3 = new Z3Context(new Z3Config("MODEL" -> true))
+  var pealProgramParser: PealProgramParser = null
 
   private def getPealProgramParser(input: String) = {
     val charStream = new ANTLRStringStream(input)
@@ -22,29 +23,29 @@ class EagerSynthesiserActor extends Actor {
   def receive = {
     case input: String =>
 
-      val pealProgramParser = getPealProgramParser(input)
+      pealProgramParser = getPealProgramParser(input)
       pealProgramParser.program()
 
       val predicateNames: Seq[String] = pealProgramParser.pols.values().flatMap(pol => pol.rules).map(r => r.q.name).toSeq.distinct
       val constsMap = predicateNames.toSeq.distinct.map(t => (t, z3.mkBoolConst(t))).toMap
       val domainSpecifics = input.split("\n").dropWhile(!_.startsWith("DOMAIN_SPECIFICS")).takeWhile(!_.startsWith("ANALYSES")).drop(1)
-      val conds = pealProgramParser.conds.toMap
-      val analyses = pealProgramParser.analyses.toMap
-
       val predicateDeclarations = for (name <- constsMap.keys) yield "(declare-const " + name + " Bool)\n"
-      val condDeclarations = for (name <- conds.keys) yield "(declare-const " + name + " Bool)\n"
-      val sortedKeys = conds.keys.toSeq.sortWith(_ < _)
+      val condDeclarations = for (name <- pealProgramParser.conds.keys) yield "(declare-const " + name + " Bool)\n"
+      val sortedKeys = pealProgramParser.conds.keys.toSeq.sortWith(_ < _)
       val body = for (cond <- sortedKeys) yield {
-        "(assert (= " + cond + " " + conds(cond).synthesis(z3, constsMap) + "))\n"
+        "(assert (= " + cond + " " + pealProgramParser.conds(cond).synthesis(z3, constsMap) + "))\n"
       }
-      val sortedAnalyses = analyses.keys.toSeq.sortWith(_ < _)
+      val sortedAnalyses = pealProgramParser.analyses.keys.toSeq.sortWith(_ < _)
       val generatedAnalyses = for (analysis <- sortedAnalyses) yield {
-        "(echo \"Result of analysis [" + analyses(analysis).analysisName + "]:\")\n" + analyses(analysis).z3SMTInput
+        "(echo \"Result of analysis [" + pealProgramParser.analyses(analysis).analysisName + "]:\")\n" + pealProgramParser.analyses(analysis).z3SMTInput
       }
 
       val z3SMTInput = predicateDeclarations.mkString("") + condDeclarations.mkString("") + body.mkString("") + domainSpecifics.mkString("", "\n", "\n") + generatedAnalyses.mkString("")
       sender ! z3SMTInput
 
-    case TimedOut => z3.delete()
+    case TimedOut =>
+      if (pealProgramParser != null) pealProgramParser = null
+      z3.delete()
+      z3 = null
   }
 }
