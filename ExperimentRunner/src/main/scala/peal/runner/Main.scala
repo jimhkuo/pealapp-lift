83c7934bf26f77009dfac3deff7221f4ef9fc200
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-08-28 10:03:53 +0100

    set up exp2 for min ez

--- ExperimentRunner/src/main/scala/peal/runner/Main.scala
+++ ExperimentRunner/src/main/scala/peal/runner/Main.scala
@@ -11,7 +11,7 @@ object Main extends App {
 
   println("Picking up z3 from environment PATH: " + System.getenv("PATH"))
 
-  binarySearchOnRuleSize(EagerOnly)
+  binarySearchOnPolicySize(EagerOnly)
 
   System.exit(0)
 
