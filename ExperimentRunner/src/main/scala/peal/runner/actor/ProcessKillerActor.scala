33ef647b5eb784dab65886dd54724d41a762dfe8
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-04 21:17:15 +0100

    set up to delete tmp file

--- ExperimentRunner/src/main/scala/peal/runner/actor/ProcessKillerActor.scala
+++ ExperimentRunner/src/main/scala/peal/runner/actor/ProcessKillerActor.scala
@@ -13,6 +13,8 @@ class ProcessKillerActor extends Actor {
       execTmp.setExecutable(true)
       val script = "foreach (`ps -A -f | grep " + file.getAbsoluteFile + " | grep nw`)  {\n@a = split;\n$pid = $a[1];\n$exists = kill 0, $pid;\n`kill -9 $pid` if ($exists);}"
       FileUtil.writeToFile(execTmp.getAbsolutePath, script)
-      Seq("perl", execTmp.getAbsolutePath).!!
+      val out = Seq("perl", execTmp.getAbsolutePath).!!
+      execTmp.delete()
+      out
   }
 }
