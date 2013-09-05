fc6cdeb3e7f3c273181fdedcb824863dc96d76ac
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-05 09:23:40 +0100

    clean up naming

--- ExperimentRunner/src/main/scala/peal/runner/actor/ProcessKillerActor.scala
+++ ExperimentRunner/src/main/scala/peal/runner/actor/ProcessKillerActor.scala
@@ -9,7 +9,7 @@ class ProcessKillerActor extends Actor {
 
   def receive = {
     case inputFile: File =>
-      val killScript = File.createTempFile("killZ3Script", "")
+      val killScript = File.createTempFile("killZ3Process", "")
       killScript.setExecutable(true)
       val script = "foreach (`ps -A -f | grep " + inputFile.getAbsoluteFile + " | grep nw`)  {\n@a = split;\n$pid = $a[1];\n$exists = kill 0, $pid;\n`kill -9 $pid` if ($exists);}"
       FileUtil.writeToFile(killScript.getAbsolutePath, script)
