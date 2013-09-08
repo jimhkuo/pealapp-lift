7f33063751d418f18791179c229dff44a76e65fc
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-08 09:06:00 +0100

    set up last exp2

--- ExperimentRunner/build.sbt
+++ ExperimentRunner/build.sbt
@@ -8,7 +8,7 @@ mainClass in assembly := Some("peal.runner.Main")
 
 name := "ExperimentRunner"
 
-version := "0.2-exp2-ez-max"
+version := "0.2-exp2-lz-mul"
 
 scalaVersion := "2.10.2"
 
--- ExperimentRunner/src/main/scala/peal/runner/Main.scala
+++ ExperimentRunner/src/main/scala/peal/runner/Main.scala
@@ -10,11 +10,11 @@ object Main extends App {
   private val timeout = 300000
 
   println("Picking up z3 from environment PATH: " + System.getenv("PATH"))
-  binarySearchOnRuleSize(EagerOnly)
+  binarySearchOnRuleSize(LazyOnly)
   System.exit(0)
 
   private def binarySearchOnRuleSize(runMode: RunMode) {
-    val execute: (Int) => Boolean = (x) => executeRunner(runMode, x, 1, 2101248/10, 1, 1, 3 * 2101248/10, 0.5, 0.1)
+    val execute: (Int) => Boolean = (x) => executeRunner(runMode, x, 1, 1, 1, 2, 3 * 2, 0.5, 0.1)
 
     var lastSuccess = 0
     var lastFailure = 0
