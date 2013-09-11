69e42b4f77f4ec3e9e7afa37f39dee4be56bf57a
hk2109 <jimhkuo@imperial.ac.uk>
2013-09-11 14:24:55 +0100

    set up new exp

--- ExperimentRunner/src/main/scala/peal/runner/Main.scala
+++ ExperimentRunner/src/main/scala/peal/runner/Main.scala
@@ -15,7 +15,7 @@ object Main extends App {
   System.exit(0)
 
   private def binarySearchOnRuleSize(runMode: RunMode) {
-    val execute: (Int) => Boolean = (x) => executeRunner(runMode, 1, x, 1, 1, 1, 3 * x, 0.5, 0.1)
+    val execute: (Int) => Boolean = (x) => executeRunner(runMode, 1, 1, x, 1, 1, 3 * x, 0.5, 0.1)
 
     var lastSuccess = 0
     var lastFailure = 0
