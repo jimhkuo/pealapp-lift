dc2f74f0c85a02c797b129a7c8097e1c19fe4796
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-11 18:49:11 +0100

    set up last exp2

--- ExperimentRunner/src/main/scala/peal/runner/Main.scala
+++ ExperimentRunner/src/main/scala/peal/runner/Main.scala
@@ -6,7 +6,7 @@ import akka.actor.ActorSystem
 
 object Main extends App {
   implicit val system = ActorSystem("system")
-  private val z3MemoryBound = 10000000
+  private val z3MemoryBound = 20000000
   private val timeout = 300000
   private val doDomainSpecifics = true
 
@@ -15,7 +15,7 @@ object Main extends App {
   System.exit(0)
 
   private def binarySearchOnRuleSize(runMode: RunMode) {
-    val execute: (Int) => Boolean = (x) => executeRunner(runMode, 1, x, 1, 1, 1, 3 * x, 0.5, 0.1)
+    val execute: (Int) => Boolean = (x) => executeRunner(runMode, 1, 1, x, 1, 1, 3 * x, 0.5, 0.1)
 
     var lastSuccess = 0
     var lastFailure = 0
