8ccb6b805e084441065a1a47490e16567a465bcf
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-19 10:10:45 +0200

    clean up

--- PealSynthesiser/src/test/scala/peal/eagersynthesis/EagerSynthesiserTest.scala
+++ PealSynthesiser/src/test/scala/peal/eagersynthesis/EagerSynthesiserTest.scala
@@ -26,12 +26,7 @@ class EagerSynthesiserTest extends ShouldMatchersForJUnit with Z3ModelMatcher {
     }
 
     val output = Await.result(outputFuture, 5000 millis)
-//    outputFuture onSuccess{
-//      case x => println(x)
-//    }
-//    val output = outputFuture.map(o => o)
 
-    println(output)
-
+    output should beZ3Model ("(declare-const q5 Bool)\n(declare-const q4 Bool)\n(declare-const q2 Bool)\n(declare-const q3 Bool)\n(declare-const q1 Bool)\n(declare-const q6 Bool)\n(declare-const cond2 Bool)\n(declare-const cond3 Bool)\n(declare-const cond4 Bool)\n(declare-const cond1 Bool)\n(assert (= cond1 (and (and (or q1 q2 q3) (or q1 q2)) (or (and (not q4) (not q5) (not q6)) (not false)))))\n(assert (= cond2 (and (or (and (not q1) (not q2) (not q3)) (not (or q1 q2))) (and (or q4 q5 q6) false))))\n(assert (= cond3 (and (or (and (not q1) (not q2) (not q3)) (not (or q1 q2))) (and (or q4 q5 q6) false))))\n(assert (= cond4 (and (or (and (not q1) (not q2) (not q3)) (not (or q1 q2))) (and (or q4 q5 q6) (and q6 q5 q4)))))\n(declare-const x Real)\n(declare-const y Real)\n(assert (= q1 (< x (+ y 1))))\n(echo \"Result of analysis [name1 = always_true? cond1]:\")\n(push)\n(declare-const always_true_name1 Bool)\n(assert (= always_true_name1 cond1))\n(assert (not always_true_name1))\n(check-sat)\n(get-model)\n(pop)\n(echo \"Result of analysis [name2 = always_false? cond1]:\")\n(push)\n(declare-const always_false_name2 Bool)\n(assert (= always_false_name2 cond1))\n(assert always_false_name2)\n(check-sat)\n(get-model)\n(pop)\n(echo \"Result of analysis [name3 = satisfiable? cond2]:\")\n(push)\n(declare-const satisfiable_name3 Bool)\n(assert (= satisfiable_name3 cond2))\n(assert satisfiable_name3)\n(check-sat)\n(get-model)\n(pop)\n(echo \"Result of analysis [name4 = equivalent? cond1 cond2]:\")\n(push)\n(declare-const equivalent_name4 Bool)\n(assert (= equivalent_name4 (or (and cond1 (not cond2)) (and (not cond1) cond2))))\n(assert equivalent_name4)\n(check-sat)\n(get-model)\n(pop)\n(echo \"Result of analysis [name5 = different? cond1 cond2]:\")\n(push)\n(declare-const different_name5 Bool)\n(assert (= different_name5 (or (and cond1 (not cond2)) (and (not cond1) cond2))))\n(assert different_name5)\n(check-sat)\n(get-model)\n(pop)")
   }
 }
