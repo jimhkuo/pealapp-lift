493951455f007560c0a4c07e5328de69468547b5
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-19 10:43:49 +0200

    check lazy synthesis output

--- PealSynthesiser/src/test/scala/peal/lazysynthesis/LazySynthesiserTest.scala
+++ PealSynthesiser/src/test/scala/peal/lazysynthesis/LazySynthesiserTest.scala
@@ -2,8 +2,9 @@ package peal.lazysynthesis
 
 import org.scalatest.junit.ShouldMatchersForJUnit
 import org.junit.Test
+import peal.util.Z3ModelMatcher
 
-class LazySynthesiserTest extends ShouldMatchersForJUnit {
+class LazySynthesiserTest extends ShouldMatchersForJUnit with Z3ModelMatcher {
 
   @Test
   def testGenerate() {
@@ -18,18 +19,16 @@ class LazySynthesiserTest extends ShouldMatchersForJUnit {
       "CONDITIONS\n" +
       "cond1 = pSet1 <= 0.5\n" +
       "cond2 = 0.6 < pSet2\n" +
-      //    "cond3 = 0.5 < pSet2\n" +
-      //    "cond4 = 0.4 < pSet2\n" +
       "ANALYSES\nname1 = always_true? cond1\n"
 
     val generator = new LazySynthesiser(input)
-    println("output:\n" + generator.generate())
+    generator.generate() should beZ3Model ("(declare-const q5 Bool)\n(declare-const q4 Bool)\n(declare-const q2 Bool)\n(declare-const q1 Bool)\n(declare-const q6 Bool)\n(declare-const cond2 Bool)\n(declare-const cond1 Bool)\n(declare-const b2_score_q4 Real)\n(assert (implies q4 (= 0.1 b2_score_q4)))\n(assert (implies (not (= 0.0 b2_score_q4)) q4))\n(declare-const b2_score_q5 Real)\n(assert (implies q5 (= 0.2 b2_score_q5)))\n(assert (implies (not (= 0.0 b2_score_q5)) q5))\n(declare-const b2_score_q6 Real)\n(assert (implies q6 (= 0.2 b2_score_q6)))\n(assert (implies (not (= 0.0 b2_score_q6)) q6))\n(declare-const cond2_b1 Bool)\n(assert (= cond2_b1 (or (and (< 0.6 1.0) (not (or q1 q2 q4)))\n(and (or q1 q2 q4) (not (or q1 q2))))))\n(declare-const cond2_b2 Bool)\n(assert (= cond2_b2 (or (and (< 0.6 0.0) (not (or q4 q5 q6)))  (and (or q4 q5 q6)  (< 0.6 (+ b2_score_q4 b2_score_q5 b2_score_q6))))))\n(assert (= cond2 (and cond2_b1 cond2_b2)))\n(declare-const cond1_b1 Bool)\n(assert (= cond1_b1 (or (and (<= 1.0 0.5) (not (or q1 q2 q4)))\n(or q1 q2))))\n(declare-const cond1_b2 Bool)\n(assert (= cond1_b2 (or (and (<=  0.0 0.5) (not (or q4 q5 q6)))  (and (or q4 q5 q6)  (<=  (+ b2_score_q4 b2_score_q5 b2_score_q6) 0.5)))))\n(assert (= cond1 (and cond1_b1 cond1_b2)))\n\n(echo \"Result of analysis [name1 = always_true? cond1]:\")\n(push)\n(declare-const always_true_name1 Bool)\n(assert (= always_true_name1 cond1))\n(assert (not always_true_name1))\n(check-sat)\n(get-model)\n(pop)")
   }
 
   @Test
   def testBroken() {
     val input = "POLICIES\nb0 = * ((q4 0.17)) default 0.95\nb1 = max ((q1 0.86)) default 0.51\nb2 = + ((q4 0.86)) default 0.99\nb3 = min ((q4 0.66) (q1 0.79)) default 0.83\nPOLICY_SETS\np0_1 = min(b0,b1)\np2_3 = min(b2,b3)\np0_3 = max(p0_1,p2_3)\n\nCONDITIONS\ncond1 = 0.50 < p0_3\ncond2 = 0.60 < p0_3\nANALYSES\nanalysis1 = always_true? cond1"
     val generator = new LazySynthesiser(input)
-    println("output:\n" + generator.generate())
+    generator.generate() should beZ3Model ("(declare-const q4 Bool)\n(declare-const q1 Bool)\n(declare-const cond2 Bool)\n(declare-const cond1 Bool)\n(declare-const b0_score_q4 Real)\n(assert (implies q4 (= 0.17 b0_score_q4)))\n(assert (implies (not (= 1.0 b0_score_q4)) q4))\n(declare-const b2_score_q4 Real)\n(assert (implies q4 (= 0.86 b2_score_q4)))\n(assert (implies (not (= 0.0 b2_score_q4)) q4))\n(declare-const cond2_b0 Bool)\n(assert (= cond2_b0 (or (and (< 0.6 0.95) (not (or q4)))  (and (or q4)  (< 0.6 (* b0_score_q4))))))\n(declare-const cond2_b1 Bool)\n(assert (= cond2_b1 (or (and (< 0.6 0.51) (not (or q1)))\n(or q1))))\n(declare-const cond2_b2 Bool)\n(assert (= cond2_b2 (or (and (< 0.6 0.99) (not (or q4)))  (and (or q4)  (< 0.6 (+ b2_score_q4))))))\n(declare-const cond2_b3 Bool)\n(assert (= cond2_b3 (or (and (< 0.6 0.83) (not (or q4 q1)))\n(and (or q4 q1) (not false)))))\n(assert (= cond2 (or (and cond2_b0 cond2_b1) (and cond2_b2 cond2_b3))))\n(declare-const cond1_b0 Bool)\n(assert (= cond1_b0 (or (and (< 0.5 0.95) (not (or q4)))  (and (or q4)  (< 0.5 (* b0_score_q4))))))\n(declare-const cond1_b1 Bool)\n(assert (= cond1_b1 (or (and (< 0.5 0.51) (not (or q1)))\n(or q1))))\n(declare-const cond1_b2 Bool)\n(assert (= cond1_b2 (or (and (< 0.5 0.99) (not (or q4)))  (and (or q4)  (< 0.5 (+ b2_score_q4))))))\n(declare-const cond1_b3 Bool)\n(assert (= cond1_b3 (or (and (< 0.5 0.83) (not (or q4 q1)))\n(and (or q4 q1) (not false)))))\n(assert (= cond1 (or (and cond1_b0 cond1_b1) (and cond1_b2 cond1_b3))))\n\n(echo \"Result of analysis [analysis1 = always_true? cond1]:\")\n(push)\n(declare-const always_true_analysis1 Bool)\n(assert (= always_true_analysis1 cond1))\n(assert (not always_true_analysis1))\n(check-sat)\n(get-model)\n(pop)")
   }
 }
