ec031a97c4ec0a48f41890ec2b658344fe5a4397
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-20 12:02:01 +0200

    tidy up Rule

--- Domain/src/main/scala/peal/domain/Rule.scala
+++ Domain/src/main/scala/peal/domain/Rule.scala
@@ -4,5 +4,4 @@ class Rule(val q: Predicate, val attribute : Either[BigDecimal, String]) {
   def this(q: Predicate, doubleScore: Double) = this(q, Left(BigDecimal.valueOf(doubleScore)))
   override def toString = "(" + q.name + " " + attribute.fold(score => score.toString(), variable => variable) + ")"
   def score = attribute.fold(score => score, variable => BigDecimal.valueOf(-999))
-  def variable = attribute.right.get
 }
