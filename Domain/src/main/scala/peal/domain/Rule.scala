530745765305a0638e0154c877a9d961c59b3ffb
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-09-20 11:42:12 +0200

    clean up rule

--- Domain/src/main/scala/peal/domain/Rule.scala
+++ Domain/src/main/scala/peal/domain/Rule.scala
@@ -2,7 +2,7 @@ package peal.domain
 
 class Rule(val q: Predicate, val attribute : Either[BigDecimal, String]) {
   def this(q: Predicate, doubleScore: Double) = this(q, Left(BigDecimal.valueOf(doubleScore)))
-  override def toString = "(" + q.name + " " + attribute.left.get + ")"
+  override def toString = "(" + q.name + " " + attribute.fold(score => score.toString(), variable => variable) + ")"
   def score = attribute.left.get
   def variable = attribute.right.get
 }
