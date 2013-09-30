2932f96625d59d0f37f74165184fa46d4a893863
hk2109 <jimhkuo@imperial.ac.uk>
2013-09-30 15:23:29 +0100

    added notes

---
+++ Domain/src/main/scala/peal/synthesis/analysis/RobustGrant.scala
@@ -0,0 +1,14 @@
+package peal.synthesis.analysis
+
+case class RobustGrant(name: String, cond: String) extends AnalysisGenerator {
+
+  //TODO need to do
+//  (and (and (sub(phi[cond])) (and (implies q1' q1) ... (implies qk' qk)))
+//  (not phi[cond]))
+
+  //need to do synthesis, then create sub(.)
+  def z3SMTInput: String = ""
+
+  def analysisName: String = name + " = robust_grant? " + cond
+
+}
