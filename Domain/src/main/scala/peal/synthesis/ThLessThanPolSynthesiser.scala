372d86ff48e1cd4c710581de9df9b992aaa1bc9a
Jim Kuo <jimhkuo@imperial.ac.uk>
2013-10-14 11:14:03 +0100

    minor clean up

--- Domain/src/main/scala/peal/synthesis/ThLessThanPolSynthesiser.scala
+++ Domain/src/main/scala/peal/synthesis/ThLessThanPolSynthesiser.scala
@@ -1,6 +1,5 @@
 package peal.synthesis
 
-import peal.domain.Pol
 import scala.collection.JavaConversions._
 import peal.domain.z3._
 import peal.domain.Pol
@@ -16,16 +15,16 @@ class ThLessThanPolSynthesiser(pol: Pol, th: BigDecimal) extends Condition {
   class ThLessThanDefault(pol: Pol, th: BigDecimal)  {
     def synthesis(consts: Map[String, PealAst]): PealAst = pol.rules.size match {
       case 0 => True()
-      case 1 => Not(consts(pol.rules(0).q.name)) //pol.rules.map("(not " + _.q.name + ")").mkString(" ")
-      case s if s > 1 => And(pol.rules.map(p => Not(consts(p.q.name))):_*) //pol.rules.map("(not " + _.q.name + ")").mkString("(and ", " ", ")")
+      case 1 => Not(consts(pol.rules(0).q.name))
+      case s if s > 1 => And(pol.rules.map(p => Not(consts(p.q.name))):_*)
     }
   }
 
   class DefaultLessThanTh(pol: Pol, th: BigDecimal)  {
     def synthesis(consts: Map[String, PealAst]): PealAst = pol.rules.size match {
       case 0 => False()
-      case 1 => consts(pol.rules(0).q.name)//pol.rules.map(_.q.name).mkString(" ")
-      case s if s > 1 => Or(pol.rules.map(p => consts(p.q.name)):_*) //pol.rules.map(_.q.name).mkString("(or ", " ", ")")
+      case 1 => consts(pol.rules(0).q.name)
+      case s if s > 1 => Or(pol.rules.map(p => consts(p.q.name)):_*)
     }
   }
   def getPol = None
