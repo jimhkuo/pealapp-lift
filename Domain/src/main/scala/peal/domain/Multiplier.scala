ba2dcbcdc797ef256d4d4cde5f4bc2a8fadb6b4b
hk2109 <jimhkuo@imperial.ac.uk>
2013-10-25 15:59:09 +0100

    adding multiplier functionality

--- Domain/src/main/scala/peal/domain/Multiplier.scala
+++ Domain/src/main/scala/peal/domain/Multiplier.scala
@@ -2,10 +2,16 @@ package peal.domain
 
 //TODO need to expand this to support 0.5*x + y
 object Multiplier {
-  def apply(n: String) = new Multiplier(1, n)
-  def apply(multiplier: BigDecimal, n: String) = new Multiplier(multiplier, n)
+  def apply(name: String) = new Multiplier(1, name)
+
+  def apply(num: BigDecimal) = new Multiplier(num)
+
+  def apply(multiplier: BigDecimal, name: String) = new Multiplier(multiplier, name)
 }
 
-class Multiplier(val multiplier: BigDecimal, val name: String) {
-  def toZ3Expression = if (multiplier != 1) "(* " + multiplier + " " + name + ")" else name
+class Multiplier(val multiplier: BigDecimal, val name: String = "") {
+  def toZ3Expression = name match {
+    case "" => multiplier.toString()
+    case _ => if (multiplier != 1) "(* " + multiplier + " " + name + ")" else name
+  }
 }
---
+++ Domain/src/test/scala/peal/domain/MultiplierTest.scala
@@ -0,0 +1,14 @@
+package peal.domain
+
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Test
+
+class MultiplierTest extends ShouldMatchersForJUnit {
+
+  @Test
+  def testSimpleScoreExpression() {
+      Multiplier(0.5).toZ3Expression should be ("0.5")
+      Multiplier("z").toZ3Expression should be ("z")
+      Multiplier(0.6, "z").toZ3Expression should be ("(* 0.6 z)")
+  }
+}
