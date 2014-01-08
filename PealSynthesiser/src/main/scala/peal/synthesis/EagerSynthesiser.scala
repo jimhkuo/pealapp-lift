67413c056751f8c078557842771e92d9137afdab
jkuo <jkuo@samurai>
2014-01-08 11:27:46 +0000

    clean up explicit synthesis a bit

--- PealSynthesiser/src/main/scala/peal/synthesis/EagerSynthesiser.scala
+++ PealSynthesiser/src/main/scala/peal/synthesis/EagerSynthesiser.scala
@@ -14,13 +14,13 @@ class EagerSynthesiser(input:String) extends Synthesiser{
     val constsMap = predicateNames.map(t => (t, Term(t))).toMap
     val predicateDeclarations = for (name <- constsMap.keys) yield "(declare-const " + name + " Bool)\n"
     val condDeclarations = for (name <- pealProgramParser.conds.keys) yield "(declare-const " + name + " Bool)\n"
-    val sortedKeys = pealProgramParser.conds.keys.toSeq.sortWith(_ < _)
+    val sortedConditions = pealProgramParser.conds.keys.toSeq.sortWith(_ < _)
+    val body = for (cond <- sortedConditions) yield {
+      "(assert (= " + cond + " " + pealProgramParser.conds(cond).synthesis(constsMap) + "))\n"
+    }
     val sortedAnalyses = pealProgramParser.analyses.keys.toSeq.sortWith(_ < _)
     val generatedAnalyses = for (analysis <- sortedAnalyses) yield {
       "(echo \"Result of analysis [" + pealProgramParser.analyses(analysis).analysisName + "]:\")\n" + pealProgramParser.analyses(analysis).z3SMTInput
-    }
-    val body = for (cond <- sortedKeys) yield {
-      "(assert (= " + cond + " " + pealProgramParser.conds(cond).synthesis(constsMap) + "))\n"
     }
     val domainSpecifics = input.split("\n").dropWhile(!_.startsWith("DOMAIN_SPECIFICS")).takeWhile(!_.startsWith("ANALYSES")).drop(1)
     predicateDeclarations.mkString("") + condDeclarations.mkString("") + body.mkString("") + domainSpecifics.mkString("", "\n", "\n") + generatedAnalyses.mkString("")
