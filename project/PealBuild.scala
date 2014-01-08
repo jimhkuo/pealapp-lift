6456e65f9b8b04caefcaf8b434cd80fa697516a7
jkuo <jkuo@samurai>
2014-01-08 11:13:54 +0000

    remove unused input checker

--- project/PealBuild.scala
+++ project/PealBuild.scala
@@ -5,15 +5,13 @@ import Keys._
 
 object PealBuild extends Build {
 
-  lazy val runner = Project(id = "ExperimentRunner", base = file("ExperimentRunner")) dependsOn(generator, synthesiser, inputChecker)
+  lazy val runner = Project(id = "ExperimentRunner", base = file("ExperimentRunner")) dependsOn(generator, synthesiser)
 
-  lazy val root = Project(id = "PealApp-lift", base = file(".")) aggregate (generator, domain, parser, synthesiser, inputChecker) dependsOn(inputChecker, generator, synthesiser, domain % "test->test;compile->compile")
+  lazy val root = Project(id = "PealApp-lift", base = file(".")) aggregate (generator, domain, parser, synthesiser) dependsOn(generator, synthesiser, domain % "test->test;compile->compile")
 
   lazy val synthesiser = Project(id = "PealSynthesiser", base = file("PealSynthesiser")) dependsOn(parser, domain % "test->test;compile->compile")
 
   lazy val generator = Project(id = "PealModelGenerator", base = file("PealModelGenerator")) dependsOn (parser, domain % "test->test;compile->compile")
-
-  lazy val inputChecker = Project(id = "InputChecker", base = file("InputChecker")) dependsOn (parser, domain % "test->test;compile->compile")
 
   lazy val parser = Project(id = "PealParser", base = file("PealParser")) dependsOn (domain % "test->test;compile->compile")
 
