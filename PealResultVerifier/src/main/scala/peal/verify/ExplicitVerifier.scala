0d6faf9fc4cf6bebb3dbb54b2983141f60ae8e37
jkuo <jkuo@samurai>
2014-01-09 12:29:06 +0000

    added useful verification notes

--- PealResultVerifier/src/main/scala/peal/verify/ExplicitVerifier.scala
+++ PealResultVerifier/src/main/scala/peal/verify/ExplicitVerifier.scala
@@ -7,4 +7,10 @@ object ExplicitVerifier {
   //extract sat/unsat result from Z3 model to create v
   //need to pass in cond map as some Conditions only hold cond names
 
+  //can take v from the truth assignment for the condition in the raw result, but need to perform external check first
+  //always_true? c1 -> c1 has to be false
+  //always_false? c1 -> c1 has to be true
+  //different? c1 c2 -> c1, c2 have to be different
+  //equivalent? c1 c2 -> c1, c2 have to be same
+  //implies? c1 c2 -> c1 has to be true, c2 has to be false
 }
