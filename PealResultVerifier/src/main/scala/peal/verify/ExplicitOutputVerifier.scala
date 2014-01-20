bd4ccaf2382cc38e9b1a6cdcdcf4ca12671cb5c1
jkuo <jkuo@samurai>
2014-01-20 13:31:17 +0000

    minor clean up

--- PealResultVerifier/src/main/scala/peal/verify/ExplicitOutputVerifier.scala
+++ PealResultVerifier/src/main/scala/peal/verify/ExplicitOutputVerifier.scala
@@ -26,11 +26,6 @@ class ExplicitOutputVerifier(input: String) {
   val analyses = pealProgramParser.analyses
   val predicateNames: Seq[String] = pealProgramParser.pols.values().flatMap(pol => pol.rules).map(r => r.q.name).toSeq.distinct
 
-  //TODO
-  //extract predicate truth assignments from Z3 model to create I
-  //take v from the truth assignment for the condition in the raw Z3 result, but need to perform external check first, see below (temporarily skipped)
-  //need to pass in cond map as some Conditions only hold cond names
-
   //always_true? c1 -> c1 has to be false
   //always_false? c1 -> c1 has to be true
   //different? c1 c2 -> c1, c2 have to be different
@@ -39,12 +34,8 @@ class ExplicitOutputVerifier(input: String) {
 
   //looking for q_i not available in I returns bottom
 
-  //need to sets up 3 way truth value, true, false, and bottom
-  //need to pull out the analyses so we can find out what condition needs to be examined
-
   def verifyModel(rawModel: String, analysisName: String): ThreeWayBoolean = {
 
-    //this ignores the correct conversion for non boolean types
     val truthMapping = ExplicitOutputProcessor.assignmentExtractor(rawModel)(analysisName).defines.map(d => (d.name, ThreeWayBooleanObj.from(d.value))).toMap
 //    println("I: " + truthMapping)
 //    println("########### Doing : " + analysisName)
