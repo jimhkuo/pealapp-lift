d480ba387c5c311ee96dabe70964174cfbcb7e9e
jkuo <jkuo@samurai>
2014-02-07 09:42:42 +0000

    can do not

--- PealSynthesiser/src/main/scala/peal/synthesis/ConditionTranslator.scala
+++ PealSynthesiser/src/main/scala/peal/synthesis/ConditionTranslator.scala
@@ -2,8 +2,9 @@ package peal.synthesis
 
 object ConditionTranslator {
 
-  def translate(cond:Condition) : String = cond match {
+  def translate(cond:Condition, conds : Map[String, Condition]) : String = cond match {
     case c: PredicateCondition => c.predicateName
+    case c: NotCondition => "(not " + translate(conds(c.condName), conds) + ")"
     case _=> "not done"
   }
 
--- PealSynthesiser/src/test/scala/peal/synthesis/ConditionTranslatorTest.scala
+++ PealSynthesiser/src/test/scala/peal/synthesis/ConditionTranslatorTest.scala
@@ -5,8 +5,15 @@ import org.junit.Test
 
 class ConditionTranslatorTest extends ShouldMatchersForJUnit {
 
+  val conds = Map("cond1" -> PredicateCondition("q"))
+
   @Test
   def testPredicate() {
-     ConditionTranslator.translate(new PredicateCondition("q")) should be ("q")
+     ConditionTranslator.translate(new PredicateCondition("q"), conds) should be ("q")
+  }
+
+  @Test
+  def testNot() {
+    ConditionTranslator.translate(new NotCondition("cond1"), conds) should be ("(not q)")
   }
 }
