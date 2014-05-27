f8b698ae68aad57e83ba4a36685e112e868112aa
jkuo <jkuo@samurai>
2014-05-27 10:54:16 +0100

    hooking up cert

--- PealResultVerifier/src/main/scala/peal/extended/ExtendedOutputVerifier.scala
+++ PealResultVerifier/src/main/scala/peal/extended/ExtendedOutputVerifier.scala
@@ -3,6 +3,14 @@ package peal.extended
 import peal.antlr.util.ParserHelper
 import scala.collection.JavaConversions._
 import peal.verifier.Z3ModelExtractor
+import peal.domain.{PealTrue, PealFalse, ThreeWayBoolean}
+import peal.synthesis.analysis._
+import peal.synthesis.analysis.AlwaysFalse
+import peal.synthesis.analysis.AlwaysTrue
+import peal.synthesis.analysis.Different
+import peal.synthesis.analysis.Satisfiable
+import peal.synthesis.analysis.Equivalent
+import peal.synthesis.Condition
 
 
 class ExtendedOutputVerifier(input: String) {
@@ -14,9 +22,52 @@ class ExtendedOutputVerifier(input: String) {
   val predicateNames: Seq[String] = pealProgramParser.pols.values().flatMap(pol => pol.rules).map(r => r.q.name).toSeq.distinct
 
   def verifyModel(rawModel: String, analysisName: String) {
-    val truthMapping = Z3ModelExtractor.extractI(rawModel)(analysisName)
-    println(truthMapping)
+    val I = Z3ModelExtractor.extractI(rawModel)(analysisName)
+    println(I)
+    doAnalysis(analysisName, I)
   }
 
+  def doAnalysis(analysisName: String, truthMapping: Map[String, Either[BigDecimal, ThreeWayBoolean]]): ThreeWayBoolean = {
+    analyses(analysisName) match {
+      case AlwaysTrue(analysisName, condName) =>
+        if (truthMapping(condName) == Right(PealFalse)) {
+          return cert(conds(condName), truthMapping, truthMapping(condName).right.get)
+        }
+        throw new RuntimeException(condName + " should be false but is not in " + analysisName)
+      case AlwaysFalse(analysisName, condName) =>
+        if (truthMapping(condName) == Right(PealTrue)) {
+          return cert(conds(condName), truthMapping, truthMapping(condName).right.get)
+        }
+        throw new RuntimeException(condName + " should be true but is not in " + analysisName)
+      case Satisfiable(analysisName, condName) =>
+        if (truthMapping(condName) == Right(PealTrue)) {
+          return cert(conds(condName), truthMapping, truthMapping(condName).right.get)
+        }
+        throw new RuntimeException(condName + " should be true but is not in " + analysisName)
+      case Different(analysisName, lhs, rhs) =>
+        if (truthMapping(lhs) != truthMapping.get(rhs)) {
+          return cert(conds(lhs), truthMapping, truthMapping(lhs).right.get) && cert(conds(rhs), truthMapping, truthMapping(rhs).right.get)
+        }
+        throw new RuntimeException(lhs + " and " + rhs + " should be different but are not in " + analysisName)
+      case Equivalent(analysisName, lhs, rhs) =>
+        if (truthMapping(lhs) != truthMapping.get(rhs)) {
+          return cert(conds(lhs), truthMapping, truthMapping(lhs).right.get) && cert(conds(rhs), truthMapping, truthMapping(rhs).right.get)
+        }
+        throw new RuntimeException(lhs + " and " + rhs + " should be different but are not in " + analysisName)
+      case Implies(analysisName, lhs, rhs) =>
+        if (truthMapping(lhs) == Right(PealTrue) && truthMapping(rhs) == Right(PealFalse)) {
+          return cert(conds(lhs), truthMapping, truthMapping(lhs).right.get) && cert(conds(rhs), truthMapping, truthMapping(rhs).right.get)
+        }
+        throw new RuntimeException(lhs + " should be true and " + rhs + " should be false, but are not in " + analysisName)
+      case _ =>
+        throw new RuntimeException("shouldn't get here, no matching analysis found")
+    }
+
+    throw new RuntimeException("shouldn't get here, no supported analysis specified")
+  }
+
+  def cert(cond: Condition, I: Map[String, Either[BigDecimal, ThreeWayBoolean]], v: ThreeWayBoolean): ThreeWayBoolean = {
+    PealFalse
+  }
 
 }
