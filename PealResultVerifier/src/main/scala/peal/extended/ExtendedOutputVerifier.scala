cf6364dcefc4b46772693d9f83db8797d5edefdb
jkuo <jkuo@samurai>
2014-05-27 10:16:06 +0100

    wip on setting up tests for extended cert

--- PealResultVerifier/src/main/scala/peal/extended/ExtendedOutputVerifier.scala
+++ PealResultVerifier/src/main/scala/peal/extended/ExtendedOutputVerifier.scala
@@ -1,5 +1,22 @@
 package peal.extended
 
-class ExtendedOutputVerifier {
+import peal.antlr.util.ParserHelper
+import scala.collection.JavaConversions._
+import peal.verifier.Z3ModelExtractor
+
+
+class ExtendedOutputVerifier(input: String) {
+  val pealProgramParser = ParserHelper.getPealParser(input)
+  pealProgramParser.program()
+
+  val conds = pealProgramParser.conds.toMap
+  val analyses = pealProgramParser.analyses
+  val predicateNames: Seq[String] = pealProgramParser.pols.values().flatMap(pol => pol.rules).map(r => r.q.name).toSeq.distinct
+
+  def cert(rawModel: String) {
+    val truthMapping = Z3ModelExtractor.extractI(rawModel)
+    println(truthMapping)
+  }
+
 
 }
