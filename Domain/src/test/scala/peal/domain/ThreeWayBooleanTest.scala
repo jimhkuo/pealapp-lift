f91f5616272647b8a4cd91b2d5d9723f0d7c435a
hk2109 <jimhkuo@imperial.ac.uk>
2014-05-28 16:46:27 +0100

    clean up test

--- Domain/src/test/scala/peal/domain/ThreeWayBooleanTest.scala
+++ Domain/src/test/scala/peal/domain/ThreeWayBooleanTest.scala
@@ -18,25 +18,25 @@ class ThreeWayBooleanTest extends ShouldMatchersForJUnit {
   def testCheckConjunction() {
     PealTrue && PealTrue should be(PealTrue)
     PealTrue && PealFalse should be(PealFalse)
+    PealTrue && PealBottom should be(PealBottom)
     PealFalse && PealTrue should be(PealFalse)
     PealFalse && PealFalse should be(PealFalse)
+    PealFalse && PealBottom should be(PealFalse)
     PealBottom && PealBottom should be(PealBottom)
     PealBottom && PealTrue should be(PealBottom)
     PealBottom && PealFalse should be(PealFalse)
-    PealTrue && PealBottom should be(PealBottom)
-    PealFalse && PealBottom should be(PealFalse)
   }
 
   @Test
   def testCheckDisjunction() {
     PealTrue || PealTrue should be(PealTrue)
     PealTrue || PealFalse should be(PealTrue)
+    PealTrue || PealBottom should be(PealTrue)
     PealFalse || PealTrue should be(PealTrue)
     PealFalse || PealFalse should be(PealFalse)
+    PealFalse || PealBottom should be(PealBottom)
     PealBottom || PealBottom should be(PealBottom)
     PealBottom || PealTrue should be(PealTrue)
     PealBottom || PealFalse should be(PealBottom)
-    PealTrue || PealBottom should be(PealTrue)
-    PealFalse || PealBottom should be(PealBottom)
   }
 }
