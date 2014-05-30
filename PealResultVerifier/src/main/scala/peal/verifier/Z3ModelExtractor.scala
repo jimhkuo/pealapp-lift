a02da2c791d981d4b079d5e10f3df7a580387e35
hk2109 <jimhkuo@imperial.ac.uk>
2014-05-30 13:16:33 +0100

    wip to use rational in verification

--- PealResultVerifier/src/main/scala/peal/verifier/Z3ModelExtractor.scala
+++ PealResultVerifier/src/main/scala/peal/verifier/Z3ModelExtractor.scala
@@ -11,4 +11,11 @@ object Z3ModelExtractor {
       case (name, z3Model) => (name, z3Model.assignments.map(assignment => (assignment.name, Z3ModelValueParser.parse(assignment.value))).toMap)
     }
   }
+
+  def extractIToRational(model: String) = {
+    val z3OutputParser = ParserHelper.getZ3OutputParser(model.mkString(""))
+    z3OutputParser.results().map {
+      case (name, z3Model) => (name, z3Model.assignments.map(assignment => (assignment.name, Z3ModelValueParser.parseToRational(assignment.value))).toMap)
+    }
+  }
 }
