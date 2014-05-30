8acff83b353201214fcbf1b8aae7abb3156077e7
hk2109 <jimhkuo@imperial.ac.uk>
2014-05-30 11:25:39 +0100

    set up Rational

--- PealResultVerifier/src/main/scala/peal/verifier/util/Rational.scala
+++ PealResultVerifier/src/main/scala/peal/verifier/util/Rational.scala
@@ -1,7 +1,10 @@
 package peal.verifier.util
 
-class Rational(numerator: String, denominator: String) {
+object Rational {
+  def apply(num: String) = new Rational(num, "1")
+//  def apply(enum: String, denom: String) = new Rational(enum, denom)
+}
 
+case class Rational(numerator: String, denominator: String) {
   def value = BigDecimal.valueOf(numerator.toDouble / denominator.toDouble)
-
 }
