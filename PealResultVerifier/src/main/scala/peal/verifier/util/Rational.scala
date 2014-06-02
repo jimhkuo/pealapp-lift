1b4d8057bafe3d35964d6bda9192db83aaf7336f
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-02 09:51:44 +0100

    checking negative numbers

--- PealResultVerifier/src/main/scala/peal/verifier/util/Rational.scala
+++ PealResultVerifier/src/main/scala/peal/verifier/util/Rational.scala
@@ -1,11 +1,16 @@
 package peal.verifier.util
 
 object Rational {
-  private def gcd(a: BigDecimal, b: BigDecimal) : BigDecimal = if (b == 0) a else gcd(b, a % b)
-  def apply(num: String) : Rational = Rational(num, "1")
+  private def gcd(a: BigDecimal, b: BigDecimal): BigDecimal = {
+//    println(a + " " + b)
+    if (b == 0) a else gcd(b, a % b)
+  }
+
+  def apply(num: String): Rational = Rational(num, "1")
+
   def apply(enum: String, denom: String) = new Rational(enum.toDouble / gcd(enum.toDouble, denom.toDouble), denom.toDouble / gcd(enum.toDouble, denom.toDouble))
 }
 
 case class Rational(numerator: BigDecimal, denominator: BigDecimal) {
-  def value : BigDecimal = numerator / denominator
+  def value: BigDecimal = numerator / denominator
 }
--- PealResultVerifier/src/test/scala/peal/verifier/util/RationalTest.scala
+++ PealResultVerifier/src/test/scala/peal/verifier/util/RationalTest.scala
@@ -8,9 +8,12 @@ class RationalTest extends ShouldMatchersForJUnit {
 
   @Test
   def testEquivalency() {
+    Rational("-1.5") should be(Rational(3, -2))
+    Rational("1.5","-1") should be(Rational(3, -2))
     Rational("1.0", "2.0") should be(Rational("1", "2"))
-    Rational("1.5") should be(Rational(3, 2))
     Rational("1.5") should be(Rational("3", "2"))
+    Rational("-1.5") should be(Rational("-3", "2"))
+    Rational("-1.5") should be(Rational("3", "-2"))
     Rational("3", "2") should be(Rational("3.0", "2"))
     Rational("3", "2") should be(Rational("1.5", "1.0"))
     Rational("30", "20") should be(Rational("15", "10"))
