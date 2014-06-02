cdb19c9cbac2b652c821e665a5c87d16d1d614b9
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-02 09:43:21 +0100

    dealing with Rational equivalency

--- PealResultVerifier/src/test/scala/peal/verifier/util/RationalTest.scala
+++ PealResultVerifier/src/test/scala/peal/verifier/util/RationalTest.scala
@@ -7,12 +7,12 @@ import org.junit.Test
 class RationalTest extends ShouldMatchersForJUnit {
 
   @Test
-  def testCanCreate() {
-      Rational("1.0","2.0") should be (Rational("1", "2"))
-  }
-
-  @Test
-  def testDecimal() {
-      Rational("1.5") should be (Rational(3, 2))
+  def testEquivalency() {
+    Rational("1.0", "2.0") should be(Rational("1", "2"))
+    Rational("1.5") should be(Rational(3, 2))
+    Rational("1.5") should be(Rational("3", "2"))
+    Rational("3", "2") should be(Rational("3.0", "2"))
+    Rational("3", "2") should be(Rational("1.5", "1.0"))
+    Rational("30", "20") should be(Rational("15", "10"))
   }
 }
