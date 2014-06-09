93452ce9411d03a990c48f5a830a1fd69247e16c
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-09 10:23:01 +0100

    dealt with last bit

--- PealModelGenerator/src/main/scala/peal/model/ModelGenerator.scala
+++ PealModelGenerator/src/main/scala/peal/model/ModelGenerator.scala
@@ -120,11 +120,11 @@ trait ModelGenerator {
     val cond1 = "cond1 = " + "%.2f".format(th) + " < " + finalPolicySet
     val cond2 = "cond2 = " + "%.2f".format(th + delta) + " < " + finalPolicySet
 
-    val pealText = "POLICIES\n" + policies.toSeq.mkString("\n") + "\nPOLICY_SETS\n" + pSets.map(c => c._1 + " = " + c._2).mkString("\n") + "\n\n" + lastBit + "CONDITIONS\n" + cond1 + "\n" + cond2 + "\n"
-    val analyses = "analysis1 = always_true? cond1\nanalysis2 = always_false? cond2\nanalysis3 = different? cond1 cond2\n"
-    val domainSpecifics = if (doDomainSpecific) generateDomainSpecifics(k / 3, pealText) else ""
+    val policiesAndConditions = "POLICIES\n" + policies.mkString("\n") + "\nPOLICY_SETS\n" + pSets.map(c => c._1 + " = " + c._2).mkString("\n") + "\n\n" + lastBit + "CONDITIONS\n" + cond1 + "\n" + cond2 + "\n"
+    val analyses = "ANALYSES\nanalysis1 = always_true? cond1\nanalysis2 = always_false? cond2\nanalysis3 = different? cond1 cond2\n"
+    val domainSpecifics = if (doDomainSpecific) generateDomainSpecifics(k / 3, policiesAndConditions) else ""
 
-    pealText + domainSpecifics + "ANALYSES\n" + analyses
+    policiesAndConditions + domainSpecifics + analyses
   }
 
 
