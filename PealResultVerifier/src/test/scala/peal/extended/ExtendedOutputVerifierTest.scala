64b2680305d63870a0c3987eab45e69054b2754a
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-09 15:59:56 +0100

    can deal with range score for + operator

--- PealResultVerifier/src/test/scala/peal/extended/ExtendedOutputVerifierTest.scala
+++ PealResultVerifier/src/test/scala/peal/extended/ExtendedOutputVerifierTest.scala
@@ -145,6 +145,15 @@ class ExtendedOutputVerifierTest extends ShouldMatchersForJUnit {
   }
 
   @Test
+  def testPolPlusWithRange() {
+    val input = "POLICIES\nb1 = + ((q1 0.2) (q2 x [0.3, 0.9]) (q3 0.1)) default 1\nPOLICY_SETS\npSet1 = b1\nCONDITIONS\ncond1 = pSet1 <= 0.5\nANALYSES\nname1 = always_true? cond1"
+    val model = "Result of analysis [name1 = always_true? cond1]:\nsat\n(model \n  (define-fun b1_score () Real\n    (/ 3.0 4.0))\n  (define-fun q3 () Bool\n    false)\n  (define-fun q2 () Bool\n    true)\n  (define-fun b1_q2_U () Real\n    (/ 3.0 10.0))\n  (define-fun cond1 () Bool\n    false)\n  (define-fun q1 () Bool\n    true)\n  (define-fun x () Real\n    (/ 1.0 4.0))\n  (define-fun always_true_name1 () Bool\n    false)\n)"
+    println(input)
+    println(model)
+    new ExtendedOutputVerifier(input).verifyModel(model, "name1")._1 should be(PealTrue)
+  }
+
+  @Test
   def testPolMul() {
     val input = "POLICIES\nb1 = * ((q1 0.5) (q2 0.5) (q3 1)) default 0.4\nPOLICY_SETS\npSet1 = b1\nCONDITIONS\ncond1 = pSet1 <= 0.5\nANALYSES\nname1 = always_true? cond1"
     val model = "Result of analysis [name1 = always_true? cond1]:\nsat\n(model \n  (define-fun cond1 () Bool\n    false)\n  (define-fun b1_score () Real\n    1.0)\n  (define-fun q1 () Bool\n    false)\n  (define-fun q3 () Bool\n    true)\n  (define-fun q2 () Bool\n    false)\n  (define-fun always_true_name1 () Bool\n    false)\n)"
