3a0d4020d2b0ec1c35ebe5ce3ab88f13de8d6066
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-09 09:29:56 +0100

    refactoring generator by pulling out the common stuff

--- PealModelGenerator/src/main/scala/peal/model/PolGenerator.scala
+++ PealModelGenerator/src/main/scala/peal/model/PolGenerator.scala
@@ -10,10 +10,7 @@ trait PolGenerator {
 
   def createPol(op: Operator, count: Int, predicates: Seq[Predicate]): Pol = {
     val tempPredicates = Random.shuffle(predicates)
-    val rules = (0 until count).map {
-      i =>
-        new Rule(tempPredicates(i), "%.4f".format(Random.nextDouble()))
-    }
+    val rules = (0 until count).map(i => new Rule(tempPredicates(i), "%.4f".format(Random.nextDouble())))
     new Pol(rules, op, "%.4f".format(Random.nextDouble()))
   }
 
