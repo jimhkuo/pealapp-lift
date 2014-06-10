bc1a97acf91f9ff138b25626598ce0d394da7626
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-10 09:19:29 +0100

    refactored lattice builder

--- PealModelGenerator/src/main/scala/peal/model/util/LatticeCreator.scala
+++ PealModelGenerator/src/main/scala/peal/model/util/LatticeCreator.scala
@@ -1,33 +1,33 @@
 package peal.model.util
 
-import scala.collection.mutable.ListBuffer
-
 object LatticeCreator {
+
   def createLattice(n : Int) = {
     val x = n * 4
     val l = (math.log(x) / math.log(2)).floor.toInt
-    var m = math.pow(2, l).toInt
-    var layer = 0
-    val lattice = ListBuffer[Seq[(Int, Int)]]()
-    while (m != 1) {
-      if (layer == 0) {
-        val lhs = for (i <- 0 until m by 2) yield i
-        val rhs = for (i <- 0 until m by 2) yield i + 1
-        val pairs = lhs.zip(rhs)
-        lattice.append(pairs)
-        m = pairs.size
+    val m = math.pow(2, l).toInt
+
+    def buildLayers(m: Int, layers: List[Seq[(Int, Int)]]): List[Seq[(Int, Int)]] = {
+      val pairs = layers.size match {
+        case 0 =>
+          val lhs = for (i <- 0 until m by 2) yield i
+          val rhs = for (i <- 0 until m by 2) yield i + 1
+          lhs.zip(rhs)
+        case _ =>
+          val lhs = for (i <- 0 until m by 2) yield layers.last(i)._1
+          val rhs = for (i <- 0 until m by 2) yield layers.last(i + 1)._2
+          lhs.zip(rhs)
+      }
+
+      if (pairs.size > 1) {
+        buildLayers(pairs.size, layers :+ pairs)
       }
       else {
-        val lhs = for (i <- 0 until m by 2) yield lattice(layer - 1)(i)._1
-        val rhs = for (i <- 0 until m by 2) yield lattice(layer - 1)(i + 1)._2
-        val pairs = lhs.zip(rhs)
-        lattice.append(pairs)
-        m = pairs.size
+        layers :+ pairs
       }
-
-      layer += 1
     }
 
-    lattice
+    buildLayers(m, List())
   }
+
 }
--- PealModelGenerator/src/test/scala/peal/model/util/LatticeCreatorTest.scala
+++ PealModelGenerator/src/test/scala/peal/model/util/LatticeCreatorTest.scala
@@ -1,11 +1,12 @@
 package peal.model.util
 
 import org.junit.Test
+import org.scalatest.junit.ShouldMatchersForJUnit
 
-class LatticeCreatorTest {
+class LatticeCreatorTest extends ShouldMatchersForJUnit {
 
   @Test
-  def testCanBuildLattice() {
-
+  def testCanBuildLattice1Layer() {
+    LatticeCreator.createLattice(1) should be (List(Vector((0,1), (2,3)), Vector((0,3))))
   }
 }
