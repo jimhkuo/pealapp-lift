ced62e550a7f7c0ac37d884af7bc6953b6cf99f9
jkuo <jkuo@samurai>
2014-06-11 12:04:28 +0100

    clean up gcd

--- PealResultVerifier/src/main/scala/peal/verifier/util/Rational.scala
+++ PealResultVerifier/src/main/scala/peal/verifier/util/Rational.scala
@@ -1,14 +1,18 @@
 package peal.verifier.util
 
 object Rational {
-  private def gcd(a: BigDecimal, b: BigDecimal): BigDecimal = if (b == 0) a else gcd(b, a % b)
+  private def gcd(a: BigDecimal, b: BigDecimal): BigDecimal = {
+    println("in gcd: a " + a + " b " + b)
+    if (b == 0) a else gcd(b, a % b)
+  }
 
   def apply(num: String): Rational = Rational(num, "1")
 
   def apply(x: String, y: String): Rational = {
+    println("apply: " + x + " " + y)
     val numer = x.toDouble
     val denom = y.toDouble
-    val commonDivisior = gcd(numer, denom)
+    val commonDivisior = gcd(x.toDouble.abs, y.toDouble.abs)
     val a = if (numer * denom < 0) -(numer / commonDivisior).abs else (numer / commonDivisior).abs
     val b = (denom / commonDivisior).abs
     Rational(a, b)
