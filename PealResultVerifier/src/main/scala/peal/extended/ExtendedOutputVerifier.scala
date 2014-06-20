d3c3cb1e8eda4ba8b51a23d7ce2c2c5eba4bf5cd
jkuo <jkuo@samurai>
2014-06-20 15:52:21 +0100

    set up exp again

--- PealResultVerifier/src/main/scala/peal/extended/ExtendedOutputVerifier.scala
+++ PealResultVerifier/src/main/scala/peal/extended/ExtendedOutputVerifier.scala
@@ -45,7 +45,7 @@ class ExtendedOutputVerifier(input: String) {
         //        print("*")
         (s + bottomPredicates.head).foreach {
           m =>
-            println("set " + m + " to false")
+//            println("set " + m + " to false")
             //            print(m)
             truthMapping += m -> Right(PealFalse)
         }
@@ -137,7 +137,7 @@ class ExtendedOutputVerifier(input: String) {
       }
     } catch {
       case e: RuntimeException =>
-        e.printStackTrace()
+//        e.printStackTrace()
         PealBottom
     }
   }
