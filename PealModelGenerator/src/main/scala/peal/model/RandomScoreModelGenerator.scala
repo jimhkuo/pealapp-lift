652a871034f5377f97def5e6817bbdaa526b8ca1
hk2109 <jimhkuo@imperial.ac.uk>
2014-06-20 18:07:10 +0100

    disabled

--- PealModelGenerator/src/main/scala/peal/model/RandomScoreModelGenerator.scala
+++ PealModelGenerator/src/main/scala/peal/model/RandomScoreModelGenerator.scala
@@ -17,12 +17,12 @@ object RandomScoreModelGenerator extends RandomModelGenerator {
     val policiesAndConditions = policies + "\nPOLICY_SETS\n" + pSets.map(c => c._1 + " = " + c._2).mkString("\n") + "\n\n" + lastBit + conditions
     val domainSpecifics = if (doDomainSpecific) generateDomainSpecifics(k / 3, policiesAndConditions) else ""
 
-    val analysis = "analysis1 = always_true? cond1\n" + "analysis2 = always_false? cond2\n" + "analysis3 = different? cond1 cond2\n"
-    //    val analysis = Random.nextInt(3) match {
-    //      case 0 => "analysis1 = always_true? cond1\n"
-    //      case 1 => "analysis2 = always_false? cond2\n"
-    //      case 2 => "analysis3 = different? cond1 cond2\n"
-    //    }
+    //    val analysis = "analysis1 = always_true? cond1\n" + "analysis2 = always_false? cond2\n" + "analysis3 = different? cond1 cond2\n"
+    val analysis = Random.nextInt(3) match {
+      case 0 => "analysis1 = always_true? cond1\n"
+      case 1 => "analysis2 = always_false? cond2\n"
+      case 2 => "analysis3 = different? cond1 cond2\n"
+    }
     val analyses = "ANALYSES\n" + analysis
 
     policiesAndConditions + domainSpecifics + analyses
