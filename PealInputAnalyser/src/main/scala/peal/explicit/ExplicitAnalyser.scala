1ee579e90fa0471742c6d4e7384d7fff28f2667a
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-04 13:34:21 +0100

    working on analyser

--- PealInputAnalyser/src/main/scala/peal/explicit/ExplicitAnalyser.scala
+++ PealInputAnalyser/src/main/scala/peal/explicit/ExplicitAnalyser.scala
@@ -2,6 +2,8 @@ package peal.explicit
 
 import peal.antlr.util.ParserHelper
 import peal.domain._
+import peal.synthesis.{GreaterThanThCondition, LessThanThCondition, Condition}
+import peal.synthesis.analysis.{Equivalent, AlwaysTrue, AnalysisGenerator}
 import peal.verifier.Z3ModelExtractor
 
 import scala.collection.JavaConversions._
@@ -15,11 +17,44 @@ class ExplicitAnalyser(input: String) {
   private val analyses = pealProgramParser.analyses
   private val predicateNames: Seq[String] = pealProgramParser.pols.values().flatMap(pol => pol.rules).map(r => r.q.name).toSeq.distinct
 
+  private def extractPolicySet(pSet : PolicySet): List[String] = pSet match {
+    case BasicPolicySet(p, n) => extractPolicySet(p)
+    case MaxPolicySet(l, r, _) => extractPolicySet(l) ::: extractPolicySet(r)
+    case MinPolicySet(l, r, _) => extractPolicySet(l) ::: extractPolicySet(r)
+    case Pol(rs, o, s, n) => n :: Nil
+  }
+
+  //This only works for explicit synthesis outputs
+  private def pullPolicies(cond : Condition) = cond match {
+    case LessThanThCondition(lhs, rhs) => extractPolicySet(lhs) :: Nil
+    case GreaterThanThCondition(lhs, rhs) => extractPolicySet(lhs) :: Nil
+  }
+
+  private def pullCond(analysis: AnalysisGenerator) = analysis match {
+    case AlwaysTrue(_, c) => c :: Nil
+    case Equivalent(_, c1, c2) => c1 :: c2 :: Nil
+  }
+
+
   def analyse(rawModel: String, analysisName: String): String = {
     val I = Z3ModelExtractor.extractI(rawModel)(analysisName)
     //call explicit verifier
+    val (ans, reMapped) = new ExplicitOutputVerifier(input).verifyModel(rawModel, analysisName)
     //pull its remapped set, together with I to create the value map
+    var completeTruthMapping = I
+    reMapped.foreach(completeTruthMapping += _ -> Right(PealFalse))
     //then pull all policies used in the analysis, reconstruct policy descriptions
+    analyses.foreach(println(_))
+    val condNames = analyses.map(a => pullCond(a._2)).flatten
+    println(condNames)
+    val policies = for {
+      c <- condNames
+      b <- pullPolicies(conds(c))
+    } yield {
+      b
+    }
+
+    println(policies.flatten.toSet)
     ???
   }
 
--- PealInputAnalyser/src/test/scala/peal/explicit/ExplicitAnalyserTest.scala
+++ PealInputAnalyser/src/test/scala/peal/explicit/ExplicitAnalyserTest.scala
@@ -1,16 +1,27 @@
 package peal.explicit
 
-import org.junit.Test
+import org.junit.{Ignore, Test}
 import org.scalatest.junit.ShouldMatchersForJUnit
 
 
 class ExplicitAnalyserTest extends ShouldMatchersForJUnit {
 
+  @Ignore("not done")
   @Test
   def testCanOutputAnalysis() {
     val input = "POLICIES\nb1 = min ((q1 0.2) (q2 0.4)) default 1\nPOLICY_SETS\npSet1 = b1\nCONDITIONS\ncond1 = pSet1 <= 0.5\nANALYSES\nname1 = always_true? cond1"
     val model = "Result of analysis [name1 = always_true? cond1]:\nsat\n(model \n  (define-fun cond1 () Bool\n    false)\n  (define-fun q1 () Bool\n    false)\n  (define-fun q2 () Bool\n    false)\n  (define-fun always_true_name1 () Bool\n    false)\n)"
+    println(input)
     val out = new ExplicitAnalyser(input).analyse(model, "name1")
     out should be ("b1 = default 1")
+  }
+
+  @Ignore("not done")
+  @Test
+  def testCanOutputAnalysis2() {
+    val input = "POLICIES\nb1 = min ((q1 0.2) (q2 0.4) (q3 0.9)) default 1\nb2 = + ((q4 0.1) (q5 0.2) (q6 0.6)) default 0\nPOLICY_SETS\npSet1 = max(b1, b2)\npSet2 = min(b1, b2)\nCONDITIONS\ncond1 = pSet1 <= 0.5\ncond2 = 0.6 < pSet2\nANALYSES\nname2 = equivalent? cond1 cond2"
+    val model = "Result of analysis [name2 = equivalent? cond1 cond2]:\nsat\n(model \n  (define-fun cond1 () Bool\n    true)\n  (define-fun q5 () Bool\n    false)\n  (define-fun equivalent_name2 () Bool\n    true)\n  (define-fun q1 () Bool\n    true)\n  (define-fun q6 () Bool\n    false)\n  (define-fun q4 () Bool\n    false)\n  (define-fun cond2 () Bool\n    false)\n)"
+    val out = new ExplicitAnalyser(input).analyse(model, "name2")
+
   }
 }
