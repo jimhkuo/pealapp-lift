edd47ec820142071611ccdaf416220627942758a
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-05 11:38:17 +0100

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sbt assembly
-NAME`ls -tr PealApp-lift-* | tail -1`
+cd target
+NAME=`ls -tr PealApp-lift-* | tail -1`
+cd ..
 scp $NAME hk2109@markham.doc.ic.ac.uk:
-ssh hk2109@markham.doc.ic.ac.uk "ps -ef | grep PealApp | awk '{print $2}' | xargs kill"
-ssh hk2109@markham.doc.ic.ac.uk "nohup java -jar "$NAME" 55555&"
+ssh hk2109@markham.doc.ic.ac.uk "./killPeal.sh"
+command="java -jar "$NAME" 55555"
+echo $command
+ssh hk2109@markham.doc.ic.ac.uk $command&
