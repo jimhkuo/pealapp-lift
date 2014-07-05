b1d1a1b0b4054b8ba8ba9c5214b74fab46d89e53
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-05 11:40:04 +0100

    fix bug in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 sbt assembly
 cd target
 NAME=`ls -tr PealApp-lift-* | tail -1`
-cd ..
 scp $NAME hk2109@markham.doc.ic.ac.uk:
+cd ..
 ssh hk2109@markham.doc.ic.ac.uk "./killPeal.sh"
 command="java -jar "$NAME" 55555"
 echo $command
