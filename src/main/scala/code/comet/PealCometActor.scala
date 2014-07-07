e1ec25ed6a1fcac9d8369b8da24d5114f837c263
jkuo <jkuo@samurai>
2014-07-07 14:28:33 +0100

    hook up pealt

--- src/main/scala/code/comet/PealCometActor.scala
+++ src/main/scala/code/comet/PealCometActor.scala
@@ -451,7 +451,16 @@ class PealCometActor extends CometActor with Loggable {
 
       val analysedResults = Z3OutputAnalyser.execute(analyses, z3OutputModels, constsMap)
 
-      this ! Result(<pre>{verificationResults.mkString("")}</pre><pre>Analysed results:<br/>{analysedResults}</pre><pre>{z3SMTInput}</pre> <pre>Z3 Raw Output:<br/>{z3RawOutput}</pre>)
+      val unfoldedInputs = for (analysis <- sortedAnalyses) yield {
+        if (z3OutputModels(analysis).satResult == Sat) {
+          "Analysis \"" + analysis + "\" " + new InputAnalyser(inputPolicies).analyse(z3RawOutput, analysis)
+        }
+        else {
+          "Analysis \"" + analysis + "\" is UNSAT"
+        }
+      }
+
+      this ! Result(<pre>{verificationResults.mkString("")}</pre><pre>Analysed results:<br/>{analysedResults}</pre><pre>Policies specialised with respect to models generated for satisfiable analyses:<br/><br/>{unfoldedInputs.mkString("\n\n")}</pre><pre>{z3SMTInput}</pre> <pre>Z3 Raw Output:<br/>{z3RawOutput}</pre>)
     } catch {
       case e: Exception =>  dealWithIt(e)
     }
