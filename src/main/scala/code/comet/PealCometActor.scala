b26a6e330d05519324b7336ce4884c76a6dbc8f2
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-21 10:02:24 +0100

    clean up notes

--- src/main/scala/code/comet/PealCometActor.scala
+++ src/main/scala/code/comet/PealCometActor.scala
@@ -18,18 +18,18 @@ import scala.util.{Try, Failure, Success}
 import scala.xml.Text
 
 //TODO vacuity check strategy
-//0. Given a Peal input
-//1. Our synthesiser (explicit or extended, depending on which GUI button is clicked) will
-//a. Parse #0 into AST objects
-//aa. Insert analyses AST objects to do vacuity checks for all conditions
-//b. Generate Z3 code using various the selected synthesising methods
-//2. Call Z3 with code generated in #1b to get the raw Z3 output
-//3. Out Z3OutputAnalyser will, for every analysis section (Result of analysis [name = satisfiable? cond]:) in #2, do
-//a. Parse Z3 outputs into a map of "String -> Or(ThreeWayBoolean, Rational)”
-//b. Create pretty printed output from #3a
-//c. Perform certification using #0, #2, report overrides
-//d. Perform specialisation using #0, #2, and overrides from #3c
-//4. Display consolidated information from #1b, #2, #3b, #3c, and #3d
+// 0. Given a Peal input
+// 1. Our synthesiser (explicit or extended, depending on which GUI button is clicked) will
+//   a. Parse #0 into AST objects
+//   aa. Insert analyses AST objects to do vacuity checks for all conditions
+//   b. Generate Z3 code using various the selected synthesising methods
+// 2. Call Z3 with code generated in #1b to get the raw Z3 output
+// 3. Out Z3OutputAnalyser will, for every analysis section (Result of analysis [name = satisfiable? cond]:) in #2, do
+//   a. Parse Z3 outputs into a map of "String -> Or(ThreeWayBoolean, Rational)”
+//   b. Create pretty printed output from #3a
+//   c. Perform certification using #0, #2, report overrides
+//   d. Perform specialisation using #0, #2, and overrides from #3c
+// 4. Display consolidated information from #1b, #2, #3b, #3c, and #3d
 class PealCometActor extends MainBody with CometListener {
 
   def registerWith = CometServer
@@ -91,6 +91,7 @@ class PealCometActor extends MainBody with CometListener {
     }
 
     result match {
+      //v._1 comes from Peal input
       case Success(v) => callZ3AndCertifyResults(isVerbose, constsMap = v._1._1, analyses = v._1._4, z3SMTInput = v._2)
       case Failure(e) => dealWithIt(e)
     }
