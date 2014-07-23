e657ec614b3e8bf355770997e888acf7f8d3ca7a
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-23 01:09:43 +0100

    seems to work!

--- src/main/scala/code/snippet/UserSettingsSnippet.scala
+++ src/main/scala/code/snippet/UserSettingsSnippet.scala
@@ -10,20 +10,7 @@ import net.liftweb._
 import net.liftweb.http.provider.HTTPCookie
 import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
-//
-//class UserSettingsSnippet {
-//
-//  val vacuityCheck = "pealvacuitychecks"
-//
-//  def render = {
-//    val checks = CookieOptions.doVacuityChecks
-//
-//
-//    <div>
-//      {SHtml.checkbox(checks, (v) => CookieOptions.doVacuityChecks = v)}
-//    </div>
-//  }
-//}
+
 object UserSettingsSnippet {
   val vacuityCheck = "peal.vacuity.checks"
 
@@ -38,21 +25,15 @@ object UserSettingsSnippet {
       case _ => false
     }
 
-    // our process method returns a
-    // JsCmd which will be sent back to the browser
-    // as part of the response
     def process(): JsCmd= {
 
-      S.redirectTo("", () => {
+      S.redirectTo("/", () => {
         val cookie = HTTPCookie(vacuityCheck, vc.toString).setMaxAge(3600)
         println("process() " + cookie)
-        //TODO it seems this doesn't add
         S.addCookie(cookie)
       })
     }
 
-    // binding looks normal
     "name=vc" #> (SHtml.checkbox(vc, vc= _, "id" -> "vc") ++ SHtml.hidden(process))
-
   }
 }
