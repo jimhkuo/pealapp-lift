77e7b425bc60d86a821ab47dd4430f91508722d0
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-23 08:32:24 +0100

    clean up UserSettingsSnippet to use CookieOptions

--- src/main/scala/code/snippet/UserSettingsSnippet.scala
+++ src/main/scala/code/snippet/UserSettingsSnippet.scala
@@ -1,32 +1,22 @@
 package code.snippet
 
 
+import code.lib.CookieOptions
 import net.liftweb.http.js.JsCmd
-import net.liftweb.http.provider.HTTPCookie
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.util.Helpers._
-import peal.util.ConsoleLogger
 
 object UserSettingsSnippet {
   val vacuityCheck = "peal.vacuity.checks"
 
   def render = {
 
-    val boxedCookie = S.findCookie(vacuityCheck)
-    val cookieVal: String = boxedCookie.map(_.value.openOr("")).openOr("")
-    ConsoleLogger.log("UserSettingsSnippet.findCookies: " + boxedCookie + " " + cookieVal)
-
-    var vc = cookieVal match {
-      case "true" => true
-      case _ => false
-    }
+    var vc = CookieOptions.doVacuityChecks
 
     def process(): JsCmd= {
 
       S.redirectTo("/done", () => {
-        val cookie = HTTPCookie(vacuityCheck, vc.toString)
-        ConsoleLogger.log("process() " + cookie)
-        S.addCookie(cookie)
+        CookieOptions.doVacuityChecks = vc
       })
     }
 
