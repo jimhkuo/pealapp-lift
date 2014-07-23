5d28626944c82623bb637e0ee624aafc0efd4cf6
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-23 01:27:42 +0100

    hooked up cookie stuff, but still got refreshing issues

--- src/main/scala/code/snippet/Time2.scala
+++ src/main/scala/code/snippet/Time2.scala
@@ -1,30 +1,25 @@
 package code.snippet
 
-import java.util.Date
-
-import code.lib.{VCOption, CookieOptions, DependencyFactory}
+import code.lib.VCOption
 import net.liftweb.http.S
-import net.liftweb.http.provider.HTTPCookie
 import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 
 class Time2 {
-  lazy val dateString = DependencyFactory.inject[Date].openOrThrowException("wantcontent").toString
 
   val vacuityCheck = "peal.vacuity.checks"
 
   def render: CssSel = {
 
-    //This is working
     val boxedCookie = S.findCookie(vacuityCheck)
     val cookieVal = boxedCookie.map(_.value.openOr("")).openOr("") match {
       case "true" => true
       case _ => false
     }
-    println("findCookies: " + boxedCookie + " " + cookieVal)
+    println("time2.findCookies: " + boxedCookie + " " + cookieVal)
 
     VCOption.set(cookieVal)
 
-    "* *" #> ("The time now is " + dateString)
+    "* *" #> ""
   }
 }
