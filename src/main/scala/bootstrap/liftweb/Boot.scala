e4f0c9f662a88594fa3c1109fee3221f7e823c74
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-23 02:25:44 +0100

    got cookie to work with a hack

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -14,8 +14,8 @@ class Boot {
     //TODO add a new page to show user options, need a comet on that page to set SessionOption settings
     val entries: List[Menuable] = List(
       Menu.i("Editor and runner") / "index",
-      Menu.i("Debug Set") / "debug",
-      Menu.i("Debug Get") / "debug2",
+//      Menu.i("Debug Set") / "debug",
+//      Menu.i("Debug Get") / "debug2",
       Menu.i("Settings") / "settings",
       Menu.i("About PEALT") / "aboutpealt",
       Menu.i("User guide") / "userguide",
