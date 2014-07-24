7b20677d9daf5fe8ca352a8791c5ee49669b6b3c
jkuo <jkuo@samurai>
2014-07-24 14:27:23 +0100

    sort the output

--- src/main/scala/peal/gui/helper/Z3OutputAnalyser.scala
+++ src/main/scala/peal/gui/helper/Z3OutputAnalyser.scala
@@ -124,7 +124,7 @@ object Z3OutputAnalyser {
           if (verifiedModel._4.nonEmpty) section.append("Variables not defined in the Z3 model but are assumed to be 0 in this certification process are: " + verifiedModel._4 )
           section.append(<br/>)
           section.append("Policy scores statically inferred in this certification process:")
-          verifiedModel._3.foreach(m => section.append(m._1 + " has score " + m._2.fold(r => r.value, b => b)))
+          verifiedModel._3.map(m => m._1 + " has score " + m._2.fold(r => r.value, b => b)).toSeq.sorted.foreach(section.append)
           section.append(<br/>)
           if (verifiedModel._2.nonEmpty) {
             section.append("Policies in analysis [" + analysisName + "] specialised with respect to the above scenario, extended with false predicates from ")
