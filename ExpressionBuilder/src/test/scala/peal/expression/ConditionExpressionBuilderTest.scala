2caa164aa67cfac7d758b61ca0a5adb662b4a3ca
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-25 17:06:01 +0100

    done second case of ConditionExpressionBuilder

--- ExpressionBuilder/src/test/scala/peal/expression/ConditionExpressionBuilderTest.scala
+++ ExpressionBuilder/src/test/scala/peal/expression/ConditionExpressionBuilderTest.scala
@@ -14,9 +14,12 @@ class ConditionExpressionBuilderTest extends ShouldMatchersForJUnit {
   def testCanBuildGreaterThanExpression() {
     val p1 = new Pol(List(new Rule(new Predicate("q1"), "0.5")), Min, "1")
     val p2 = new Pol(List(new Rule(new Predicate("q2"), "0.5")), Min, "0")
-    val pSet = new MinPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet")
-    val phi = new GreaterThanThCondition(pSet, Left(0.6))
+    val pSet1 = new MinPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet1")
+    val pSet2 = new MaxPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet2")
+    val cond1 = new GreaterThanThCondition(pSet1, Left(0.6))
+    val cond2 = new GreaterThanThCondition(pSet1, Right(pSet2))
 
-    ConditionExpressionBuilder.build(phi) should be(pSet.getPolicySetName + " > 0.6")
+    ConditionExpressionBuilder.build(cond1) should be(pSet1.getPolicySetName + " > 0.6")
+    ConditionExpressionBuilder.build(cond2) should be(pSet1.getPolicySetName + " > " + pSet2.getPolicySetName)
   }
 }
