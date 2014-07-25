b20a1d20280caf9c11f790fde07afc1c189fd632
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-25 17:08:13 +0100

    done <= case of ConditionExpressionBuilder

--- ExpressionBuilder/src/main/scala/peal/expression/ConditionExpressionBuilder.scala
+++ ExpressionBuilder/src/main/scala/peal/expression/ConditionExpressionBuilder.scala
@@ -1,9 +1,10 @@
 package peal.expression
 
-import peal.synthesis.{GreaterThanThCondition, Condition}
+import peal.synthesis.{LessThanThCondition, GreaterThanThCondition, Condition}
 
 object ConditionExpressionBuilder {
   def build(pealObject: Condition) = pealObject match {
     case GreaterThanThCondition(lhs, rhs) => lhs.getPolicySetName + " > " + rhs.fold(b => b, pSet => pSet.getPolicySetName)
+    case LessThanThCondition(lhs, rhs) => lhs.getPolicySetName + " <= " + rhs.fold(b => b, pSet => pSet.getPolicySetName)
   }
 }
--- ExpressionBuilder/src/test/scala/peal/expression/ConditionExpressionBuilderTest.scala
+++ ExpressionBuilder/src/test/scala/peal/expression/ConditionExpressionBuilderTest.scala
@@ -4,7 +4,7 @@ import org.junit.Test
 import org.scalatest.junit.ShouldMatchersForJUnit
 import peal.domain._
 import peal.domain.operator.Min
-import peal.synthesis.GreaterThanThCondition
+import peal.synthesis.{LessThanThCondition, GreaterThanThCondition}
 import scala.collection.JavaConversions._
 
 
@@ -21,5 +21,18 @@ class ConditionExpressionBuilderTest extends ShouldMatchersForJUnit {
 
     ConditionExpressionBuilder.build(cond1) should be(pSet1.getPolicySetName + " > 0.6")
     ConditionExpressionBuilder.build(cond2) should be(pSet1.getPolicySetName + " > " + pSet2.getPolicySetName)
+  }
+
+  @Test
+  def testCanBuildLessThanExpression() {
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), "0.5")), Min, "1")
+    val p2 = new Pol(List(new Rule(new Predicate("q2"), "0.5")), Min, "0")
+    val pSet1 = new MinPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet1")
+    val pSet2 = new MaxPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet2")
+    val cond1 = new LessThanThCondition(pSet1, Left(0.6))
+    val cond2 = new LessThanThCondition(pSet1, Right(pSet2))
+
+    ConditionExpressionBuilder.build(cond1) should be(pSet1.getPolicySetName + " <= 0.6")
+    ConditionExpressionBuilder.build(cond2) should be(pSet1.getPolicySetName + " <= " + pSet2.getPolicySetName)
   }
 }
