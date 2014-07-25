5e6c545f1c18e905b7a120892c2c1dbd298a23ca
hk2109 <jimhkuo@imperial.ac.uk>
2014-07-25 17:18:42 +0100

    added and condition expression builder

--- ExpressionBuilder/src/main/scala/peal/expression/ConditionExpressionBuilder.scala
+++ ExpressionBuilder/src/main/scala/peal/expression/ConditionExpressionBuilder.scala
@@ -1,10 +1,11 @@
 package peal.expression
 
-import peal.synthesis.{LessThanThCondition, GreaterThanThCondition, Condition}
+import peal.synthesis.{AndCondition, LessThanThCondition, GreaterThanThCondition, Condition}
 
 object ConditionExpressionBuilder {
-  def build(pealObject: Condition) = pealObject match {
+  def build(pealObject: Condition, conditions: Map[String, Condition] = Map()): String = pealObject match {
     case GreaterThanThCondition(lhs, rhs) => lhs.getPolicySetName + " > " + rhs.fold(b => b, pSet => pSet.getPolicySetName)
     case LessThanThCondition(lhs, rhs) => lhs.getPolicySetName + " <= " + rhs.fold(b => b, pSet => pSet.getPolicySetName)
+    case AndCondition(lhs, rhs) => "(" + build(conditions(lhs)) + ") && (" + build(conditions(rhs)) + ")"
   }
 }
--- ExpressionBuilder/src/test/scala/peal/expression/ConditionExpressionBuilderTest.scala
+++ ExpressionBuilder/src/test/scala/peal/expression/ConditionExpressionBuilderTest.scala
@@ -4,7 +4,7 @@ import org.junit.Test
 import org.scalatest.junit.ShouldMatchersForJUnit
 import peal.domain._
 import peal.domain.operator.Min
-import peal.synthesis.{LessThanThCondition, GreaterThanThCondition}
+import peal.synthesis.{Condition, AndCondition, LessThanThCondition, GreaterThanThCondition}
 import scala.collection.JavaConversions._
 
 
@@ -34,5 +34,22 @@ class ConditionExpressionBuilderTest extends ShouldMatchersForJUnit {
 
     ConditionExpressionBuilder.build(cond1) should be(pSet1.getPolicySetName + " <= 0.6")
     ConditionExpressionBuilder.build(cond2) should be(pSet1.getPolicySetName + " <= " + pSet2.getPolicySetName)
+  }
+
+  @Test
+  def testCanBuildAndExpression() {
+    val p1 = new Pol(List(new Rule(new Predicate("q1"), "0.5")), Min, "1")
+    val p2 = new Pol(List(new Rule(new Predicate("q2"), "0.5")), Min, "0")
+    val pSet1 = new MinPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet1")
+    val pSet2 = new MaxPolicySet(new BasicPolicySet(p1), new BasicPolicySet(p2), "pSet2")
+    val cond1 = new LessThanThCondition(pSet1, Left(0.6))
+    val cond2 = new GreaterThanThCondition(pSet1, Right(pSet2))
+    val cond3 = AndCondition("cond1", "cond2")
+    val conditions = Map[String, Condition]("cond1" -> cond1, "cond2" -> cond2, "cond3" -> cond3)
+
+    ConditionExpressionBuilder.build(cond3, conditions) should be(
+      "(" + pSet1.getPolicySetName + " <= 0.6)" +
+        " && " +
+        "(" + pSet1.getPolicySetName + " > " + pSet2.getPolicySetName + ")")
   }
 }
