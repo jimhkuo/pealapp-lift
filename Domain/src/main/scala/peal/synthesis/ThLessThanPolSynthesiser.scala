c513fe481f3944c69fcacd6f98c697802dcd834c
hk2109 <jimhkuo@imperial.ac.uk>
2014-08-08 10:13:19 +0100

    start cleaning up explicit synthesis

--- Domain/src/main/scala/peal/synthesis/ThLessThanPolSynthesiser.scala
+++ Domain/src/main/scala/peal/synthesis/ThLessThanPolSynthesiser.scala
@@ -14,25 +14,15 @@ class ThLessThanPolSynthesiser(pol: Pol, th: BigDecimal) extends Condition {
       case s if s > 1 => And(pol.rules.map(p => Not(consts(p.q.name))): _*)
     }
 
-    pol.defaultNumericalScore match {
-      case s if th < s => Or(thLessThanDefault, new NonDefaultThLessThanPol(pol, th).synthesis(consts))
-      case _ => And(new DefaultLessThanTh(pol, th).synthesis(consts), new NonDefaultThLessThanPol(pol, th).synthesis(consts))
-    }
-  }
-
-  //  class ThLessThanDefault(pol: Pol, th: BigDecimal)  {
-  //    def synthesis(consts: Map[String, PealAst]): PealAst = pol.rules.size match {
-  //      case 0 => True()
-  //      case 1 => Not(consts(pol.rules(0).q.name))
-  //      case s if s > 1 => And(pol.rules.map(p => Not(consts(p.q.name))):_*)
-  //    }
-  //  }
-
-  class DefaultLessThanTh(pol: Pol, th: BigDecimal) {
-    def synthesis(consts: Map[String, PealAst]): PealAst = pol.rules.size match {
+    def defaultLessThanTh: PealAst = pol.rules.size match {
       case 0 => False()
       case 1 => consts(pol.rules(0).q.name)
       case s if s > 1 => Or(pol.rules.map(p => consts(p.q.name)): _*)
+    }
+
+    pol.defaultNumericalScore match {
+      case s if th < s => Or(thLessThanDefault, new NonDefaultThLessThanPol(pol, th).synthesis(consts))
+      case _ => And(defaultLessThanTh, new NonDefaultThLessThanPol(pol, th).synthesis(consts))
     }
   }
 
