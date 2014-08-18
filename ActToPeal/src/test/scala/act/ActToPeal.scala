da35d3cfe659d7b183ecf8e8b90123da7a1dab87
jkuo <jkuo@samurai>
2014-08-18 14:15:26 +0100

    set up act to peal converter test

--- ActToPeal/src/test/scala/act/ActToPeal.scala
+++ ActToPeal/src/test/scala/act/ActToPeal.scala
@@ -13,17 +13,16 @@ object ActToPeal {
         rhs.map(executeNext).mkString
     case OrLeaf(name, lhs, rhs@_*) =>
       "prob_or_" + name + "= +((True 1.0) (True -1.0*prob_or_" + name + "_aux_score)) default 0.0\n" +
-        "prob_or_" + name + "_aux = *((True prob_or_" + name + "_aux1_score)" + orAuxRules(name, rhs: _*).mkString + ") default 0.0\n" +
-        "prob_or_" + name + "_aux1 = +((True 1.0) (True -1.0*" + policyName(lhs) + ")) default 0.0\n" +
-        orAux(name, rhs: _*).mkString
+        "prob_or_" + name + "_aux = *(" + orAuxRules(name, Seq(lhs) ++ rhs: _*).mkString + ") default 0.0\n" +
+        orAux(name, Seq(lhs) ++ rhs: _*).mkString
     case AttLeaf(name, pred, prob, cost, impact) => "prob_att_leaf_" + name + " = +((" + pred + " " + prob + ")) default 0.0"
     case NotLeaf(name, dm) => "prob_not_" + name + " = +((True -1.0* " + policyName(dm) + ")) default 0.0"
   }
 
-  private def orAux(name: String, acts: Act*) = for ((act, i) <- acts.zipWithIndex) yield ("prob_or_" + name + "_aux" + (i + 2) + " = +((True 1.0) (True -1.0*" + policyName(act) + ")) default 0.0\n")
+  private def orAux(name: String, acts: Act*) = for ((act, i) <- acts.zipWithIndex) yield ("prob_or_" + name + "_aux" + (i + 1) + " = +((True 1.0) (True -1.0*" + policyName(act) + ")) default 0.0\n")
 
 
-  private def orAuxRules(name: String, acts: Act*) = for ((act, i) <- acts.zipWithIndex) yield ("(True prob_or_" + name + "_aux" + (i + 2) + "_score)")
+  private def orAuxRules(name: String, acts: Act*) = for ((act, i) <- acts.zipWithIndex) yield ("(True prob_or_" + name + "_aux" + (i + 1) + "_score)")
 
   private def policyName(act: Act): String = act match {
     case AndLeaf(name, _, rhs@_*) => "prob_and_" + name + "_score"
