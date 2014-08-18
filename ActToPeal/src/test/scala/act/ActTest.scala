f9486406745ed0ff259b89e85d213cad76c5e64d
jkuo <jkuo@samurai>
2014-08-18 12:46:49 +0100

    set up act to peal converter test

--- ActToPeal/src/test/scala/act/ActTest.scala
+++ ActToPeal/src/test/scala/act/ActTest.scala
@@ -29,47 +29,11 @@ class ActTest extends ShouldMatchersForJUnit {
   val or2 = OrLeaf("2", a111, or3)
   val and3 = AndLeaf("3", or2, and6)
   val and1 = AndLeaf("1", and3, not1)
-  val bgp = OrLeaf("or1", and1, and2)
+  val bgp = OrLeaf("1", and1, and2)
 
   @Test
   def testCanConstructActTreeInNotes() {
-    //    BGP_reset_session = ({ type = OR, name = "or1", children = [and1, and2] )
-    //      and1 = { type = AND, name = "1", children = [and3, not1] }
-    //      and3 = { type = AND, name = "3", comment="a1: send message to router causing reset", children = [or2, and6] }
-    //      or2 = { type = OR, name = "2", children = [a111, a112] }
-    //      a111 = { type = ATTACK_LEAF, name = "111", comment = "a111: Send RST message to TCP stack", pred = "sendRSTmessageToTCPStack", probability = 0.08, cost = 50, impact = 200 }
-    //      or3 = { type = OR, name = "3", comment="a112: Send BGP message", children = [a1121, a1122, a1123] }
-    //      a1121 = { type = ATTACK_LEAF, name = "1121", comment = "a1121: Notify", pred = "notify", probability = 0.1, cost = 60, impact = 130 }
-    //      a1122 = { type = ATTACK_LEAF, name = "1122", comment = "a1122: Open", pred = "open", probability = 0.15, cost = 70, impact = 100 }
-    //      a1123 = { type = ATTACK_LEAF, name = "1123", comment = "a1123: Keep alive", pred = "keepAlive", probability = 0.2, cost = 100, impact = 300 }
-    //      and6 = { type = AND, name = "6", children = [a12, not3] }
-    //      a12 = { type = ATTACK_LEAF, name = "12", comment = "a12: TCP sequence number attack", pred = "TCPsequenceNumberAttack", probability = 0.1, cost = 150, impact = 250 }
-    //      not3 = { type = NOT, name = "3", children = [and7] }
-    //      and7 = { type = AND, name = "7", children = [d12, m12] }
-    //      d12 = { type = DETECTION_LEAF, name = "12", comment = "d12: TCP sequence number check", pred = "TCPsequenceNumberCheck", probability = 0.8, securityInvestmentCost = 10 }
-    //      m12 = { type = MITIGATION_LEAF, name = "12", comment = "m12: MD5 authentication", pred = "MD5authentication", probability = 0.5, securityInvestmentCost = 20 }
-    //      not1 = { type = NOT, name = "1", children = [and4] }
-    //      and4 = { type = AND, name = "4", children = [d1, m1] }
-    //      d1 = { type = DETECTION_LEAF, name = "1", comment = "d1: Trace-route check", pred = "traceRouteCheck", probability = 0.5, securityInvestmentCost = 10 }
-    //      m1 = { type = MITIGATION_LEAF, name = "1", comment = "m1: Randomize sequence numbers", pred = "randomizeSequenceNumbers", probability = 0.6, securityInvestmentCost = 30 }
-    //      and2 = { type = AND, name = "2", children = [a2, not2] }
-    //      a2 = { type = ATTACK_LEAF, name = "2", comment = "a2: Alter configuration via compromised router", pred = "alterConfigurationViaCompromisedRouter", probability = 0.4, cost = 190, impact = 275 }
-    //      not2 = { type = NOT, name = "2", children = [and5] }
-    //      and5 = {type = AND, name = "5", children = [d2, m2] }
-    //      d2 = { type = DETECTION_LEAF, name = "2", comment = "d2: Router firewall alert", pred = "routerFirewallAlert", probability = 0.7, securityInvestmentCost = 15 }
-    //      m2 = { type = MITIGATION_LEAF, name = "2", comment = "m2: Secure router", pred = "secureRouter", probability = 0.5, securityInvestmentCost = 35 }
-
-
-
     println(bgp)
-
-    //    (OR, 1, (AND, 1, (AND, 3, (OR, 2, ("att", 111, sendRSTmessageToTCPStack, 0.08, 50, 200), (OR, 3, ("att", 1121, notify, 0.1, 60, 130), ("att", 1122, open, 0.15, 70, 100), ("att", 1123, keepAlive, 0.2, 100, 300))), (AND, 6, ("att", 12, TCPsequenceNumberAttack, 0.1, 150, 250), (NOT, 3, (AND, 7, ("det", 12, TCPsequenceNumberCheck, 0.8, 10), ("mit", 12, MD5authentication, 0.5, 20)))))), (NOT, 1, (AND, 4, ("det", 1, traceRouteCheck, 0.5, 10), ("mit", 1, randomizeSequenceNumbers, 0.6, 30)))), (AND, 2, ("att", 2, alterConfigurationViaCompromisedRouter, 0.4, 190, 275), (NOT, 2, (AND, 5, ("det", 2, routerFirewallAlert, 0.7, 15), ("mit", 2, secureRouter, 0.5, 35)))))
-
-    //    OrLeaf(or1,AndLeaf(1,AndLeaf(3,OrLeaf(2,AttLeaf(111,sendRSTmessageToTCPStack,0.08,50,200),WrappedArray(OrLeaf(3,AttLeaf(1121,notify,0.1,60,130),WrappedArray(AttLeaf(1122,open,0.15,70,100), AttLeaf(1123,keepAlive,0.2,100,300))))),WrappedArray(AndLeaf(6,AttLeaf(12,TCPsequenceNumberAttack,0.1,150,250),WrappedArray(NotLeaf(3,AndDmAct(7,DetLeaf(12,TCPsequenceNumberCheck,0.8,10),WrappedArray(MitLeaf(12,MD5authentication,0.5,20))),WrappedArray()))))),WrappedArray(NotLeaf(1,AndDmAct(4,DetLeaf(1,traceRouteCheck,0.5,10),WrappedArray(MitLeaf(1,randomizeSequenceNumbers,0.6,30))),WrappedArray()))),WrappedArray(AndLeaf(2,
-    // AttLeaf(2,alterConfigurationViaCompromisedRouter,0.4,190,275),WrappedArray(
-    //               NotLeaf(2,AndDmAct(5,
-    //               DetLeaf(2,routerFirewallAlert,0.7,15),WrappedArray(MitLeaf(2,secureRouter,0.5,35))),WrappedArray())))))
-
   }
 
   @Test
@@ -111,6 +75,7 @@ class ActTest extends ShouldMatchersForJUnit {
       "prob_det_leaf_2 = +((routerFirewallAlert 0.7)) default 0.0\n" +
       "prob_mit_leaf_2 = +((secureRouter 0.5)) default 0.0"
 
+    println(ActToPeal.execute(bgp))
     ActToPeal.execute(bgp) should be(expected)
   }
 
--- ActToPeal/src/test/scala/act/ActToPeal.scala
+++ ActToPeal/src/test/scala/act/ActToPeal.scala
@@ -2,7 +2,18 @@ package act
 
 object ActToPeal {
 
-  def execute(act: Act) : String = {
-    ""
+  def execute(act: Act) : String = act match {
+    case OrLeaf(name, lhs, rhs @ _*) => "attack_success_probability = +((True prob_or_" +  name + "_score)) default 0.0\n" + executeNext(lhs)
+  }
+
+  private def executeNext(act : Act) : String = act match {
+    case AndLeaf(name, lhs, rhs @ _*) => "prob_and_" + name + " = *((True " + policyName(lhs) + "_score)" + rhs.map("(True " + policyName(_) + "_score)").mkString(" ") + ") default 0.0"
+
+  }
+
+  private def policyName(act : Act) : String = act match {
+    case AndLeaf(name, _, _) => "prob_and_" + name
+    case OrLeaf(name, _, _) => "prob_or_" + name
+    case NotLeaf(name, _) => "prob_not_" + name
   }
 }
