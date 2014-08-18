6f07ce51138db3629857e13c13216216a9e917db
jkuo <jkuo@samurai>
2014-08-18 13:16:38 +0100

    set up act to peal converter test

--- ActToPeal/src/test/scala/act/ActToPeal.scala
+++ ActToPeal/src/test/scala/act/ActToPeal.scala
@@ -7,8 +7,15 @@ object ActToPeal {
   }
 
   private def executeNext(act: Act): String = act match {
-    case AndLeaf(name, lhs, rhs@_*) => "prob_and_" + name + " = *((True " + policyName(lhs) + "_score)" + rhs.map("(True " + policyName(_) + "_score)").mkString(" ") + ") default 0.0\n"
-
+    case AndLeaf(name, lhs, rhs@_*) =>
+      "prob_and_" + name + " = *((True " + policyName(lhs) + "_score)" + rhs.map("(True " + policyName(_) + "_score)").mkString(" ") + ") default 0.0\n" +
+        executeNext(lhs) +
+        rhs.map(executeNext).mkString
+    case OrLeaf(name, lhs, rhs@_*) =>
+      "prob_or_" + name + "= +((True 1.0) (True -1.0*prob_or_" + name + "_aux_score)) default 0.0\n" +
+        "prob_or_" + name + "_aux = *((True prob_or_" + name + "_aux1_score)" + ") default 0.0\n" +
+        "prob_or_" + name + "_aux1 = +((True 1.0) (True -1.0*" + policyName(lhs) + "_score)) default 0.0\n"
+    case AttLeaf(name, pred, prob, cost, impact) => "prob_att_leaf_" + name + " = +((" + pred + " " + prob + ")) default 0.0"
   }
 
   private def policyName(act: Act): String = act match {
