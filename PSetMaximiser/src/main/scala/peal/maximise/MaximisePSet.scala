8f67f880b81768dd15afd571518fc44d1be44414
jkuo <jkuo@samurai>
2014-09-01 11:09:38 +0100

    added z3 dependency

--- PSetMaximiser/src/main/scala/peal/maximise/MaximisePSet.scala
+++ PSetMaximiser/src/main/scala/peal/maximise/MaximisePSet.scala
@@ -2,8 +2,10 @@ package peal.maximise
 
 import peal.antlr.util.ParserHelper
 import peal.domain.Pol
+import peal.domain.z3.Model
 import peal.synthesis.{ExtendedSynthesiserCore, GreaterThanThCondition}
 import peal.synthesis.analysis.Satisfiable
+import peal.z3.Z3Caller
 import scala.collection.JavaConversions._
 
 
@@ -27,17 +29,19 @@ case class MaximisePSet(input : String, pSet: String, accuracy: BigDecimal, pol:
   })
   val pSets = pealProgramParser.pSets.toMap
 
-  //  val conds = pealProgramParser.conds.toMap
-  //  val analyses = pealProgramParser.analyses.toMap
-
-  private def satisfiableZ3Code(threshold: BigDecimal) = {
-    val conds: Map[String, GreaterThanThCondition] = Map("cond1" -> GreaterThanThCondition(pSets(pSet + "_" + pol), Left(0.0)))
+  private def satisfiableZ3Code(threshold: BigDecimal, pol: String) = {
+    val conds: Map[String, GreaterThanThCondition] = Map("cond1" -> GreaterThanThCondition(pSets(pSet + (if (pol != "") "_" + pol else "")), Left(0.0)))
     val analyses: Map[String, Satisfiable] = Map("name1" -> new Satisfiable("name1", "cond1"))
-    ExtendedSynthesiserCore(pols, conds, pSets, analyses, Array(), predicateNames, variableDefaultScores, variableScores).generate()
+    val z3Code = ExtendedSynthesiserCore(pols, conds, pSets, analyses, Array(), predicateNames, variableDefaultScores, variableScores).generate()
+    val z3RawOutput = Z3Caller.call(z3Code)
+    val parser = ParserHelper.getZ3OutputParser(z3RawOutput)
+    val model: Map[String, Model] = parser.results().toMap
+    model("name1").isSat
   }
 
   def doIt() = {
-    0.0
+//    satisfiableZ3Code(0.0)
+    if (pol)
   }
 
 }
