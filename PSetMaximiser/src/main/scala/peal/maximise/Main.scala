3793505ef0eb5c2b2cad492fbb71759fb346dd47
jkuo <jkuo@samurai>
2014-09-04 09:58:22 +0100

    hooked up main

--- PSetMaximiser/src/main/scala/peal/maximise/Main.scala
+++ PSetMaximiser/src/main/scala/peal/maximise/Main.scala
@@ -1,5 +1,12 @@
 package peal.maximise
 
 object Main extends App {
-  println("hello world")
+  val input = scala.io.Source.fromFile(args(0)).mkString
+  val out = args.length match {
+    case 3 => MaximisePSet(input, args(1), BigDecimal(args(2))).doIt()
+    case 4 => MaximisePSet(input, args(1), BigDecimal(args(2)), args(3)).doIt()
+    case _ => "Arguments: peal_file_name pSet accuracy [pol]"
+  }
+
+  println(out)
 }
