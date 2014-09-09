9e92fe60f0b57eee89433707e8d10a3a36127e6a
hk2109 <jimhkuo@imperial.ac.uk>
2014-09-09 09:20:27 +0100

    fixed the model satisfiability extraction

--- PSetMaximiser/src/main/scala/peal/maximise/MaximisePSet.scala
+++ PSetMaximiser/src/main/scala/peal/maximise/MaximisePSet.scala
@@ -1,6 +1,7 @@
 package peal.maximise
 
 import peal.antlr.util.ParserHelper
+import peal.domain.z3.{Unsat, Sat, SatResult}
 import peal.domain.{ThreeWayBoolean, Rational, Pol}
 import peal.synthesis.analysis.Satisfiable
 import peal.synthesis.{ExtendedSynthesiserCore, GreaterThanThCondition}
@@ -31,13 +32,13 @@ case class MaximisePSet(input: String, pSet: String, accuracy: BigDecimal, pol: 
   val pSets = pealProgramParser.pSets.toMap
   val domainSpecifics: Array[String] = input.split("\n").dropWhile(!_.startsWith("DOMAIN_SPECIFICS")).takeWhile(!_.startsWith("ANALYSES")).drop(1).filterNot(_.trim.startsWith("%"))
 
-  def runSatisfiableAnalysis(threshold: BigDecimal): Map[String, Either[Rational, ThreeWayBoolean]] = {
+  def runSatisfiableAnalysis(threshold: BigDecimal): (SatResult, Map[String, Either[Rational, ThreeWayBoolean]]) = {
     val conds: Map[String, GreaterThanThCondition] = Map("cond1" -> GreaterThanThCondition(pSets(pSet + (if (pol != "") "_" + pol else "")), Left(threshold)))
     val analyses: Map[String, Satisfiable] = Map("name1" -> new Satisfiable("name1", "cond1"))
     val z3Code = ExtendedSynthesiserCore(pols, conds, pSets, analyses, domainSpecifics, predicateNames, variableDefaultScores, variableScores).generate()
     val z3RawOutput = Z3Caller.call(z3Code)
     //TODO need to do certification here, throw exception if fails
-    val I: Map[String, Either[Rational, ThreeWayBoolean]] = Z3ModelExtractor.extractIUsingRational(z3RawOutput)("name1")._2
+    val I = Z3ModelExtractor.extractIUsingRational(z3RawOutput)("name1")
 //    println(I)
     I
   }
@@ -48,11 +49,11 @@ case class MaximisePSet(input: String, pSet: String, accuracy: BigDecimal, pol: 
     while ((high - low) > accuracy) {
       val middle = (low + high) / 2
       val I = runSatisfiableAnalysis(middle)
-      if (I.nonEmpty) {
+      if (I._1 == Sat) {
         if (pol != "") {
-          val temp = I(pol + "_score").left.get.value
-          val J = runSatisfiableAnalysis(temp)
-          if (J.isEmpty) {
+          val temp = I._2(pol + "_score").left.get.value
+          val J = runSatisfiableAnalysis(temp)._1
+          if (J == Unsat) {
             return "exact maximum is " + temp
           }
           low = temp.max(middle)
@@ -70,22 +71,22 @@ case class MaximisePSet(input: String, pSet: String, accuracy: BigDecimal, pol: 
     val I = runSatisfiableAnalysis(0.0)
     var high, low = BigDecimal(0.0)
 
-    if (I.nonEmpty) { //TODO need a better way to check status
+    if (I._1 == Sat) { //TODO need a better way to check status
       if (pol != "") {
-        val temp = I(pol + "_score").left.get.value
-        val J = runSatisfiableAnalysis(temp)
-        if (J.isEmpty) {
+        val temp = I._2(pol + "_score").left.get.value
+        val J = runSatisfiableAnalysis(temp)._1
+        if (J == Unsat) {
           return "exact maximum is " + temp
         }
       }
 
       high = low.max(2.0)
       var K = runSatisfiableAnalysis(high)
-      while (K.nonEmpty) {
+      while (K._1 == Sat) {
         if (pol != "") {
-          val temp = K(pol + "_score").left.get.value
-          val J = runSatisfiableAnalysis(temp)
-          if (J.isEmpty) {
+          val temp = K._2(pol + "_score").left.get.value
+          val J = runSatisfiableAnalysis(temp)._1
+          if (J == Unsat) {
             return "exact maximum is " + temp
           }
           low = temp.max(high)
